848e655adc31dcaa24e98267c9950945e2df1f27
Anders Åberg <anders@andersaberg.com>
2023-05-11 18:53:53 +0200

    Seed database with ApiKeys for tenant 'test'

--- tests/Api.IntegrationTests/TestWebApplicationFactory.cs
+++ tests/Api.IntegrationTests/TestWebApplicationFactory.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Passwordless.Service.Models;
+using Passwordless.Service.Storage;
 using Passwordless.Service.Storage.Ef;
 
 namespace Passwordless.Api.IntegrationTests;
@@ -11,6 +13,8 @@ public class TestWebApplicationFactory<TProgram>
     : WebApplicationFactory<TProgram> where TProgram : class
 {
     public string DbName { get; set; } = Guid.NewGuid().ToString();
+    public string ApiKey { get; set; } = "test:public:2e728aa5986f4ba8b073a5b28a939795";
+    public string ApiSecret { get; set; } = "test:secret:a679563b331846c79c20b114a4f56d02";
 
     protected override IHost CreateHost(IHostBuilder builder)
     {
@@ -27,9 +31,16 @@ public class TestWebApplicationFactory<TProgram>
         builder.ConfigureServices(services =>
         {
             using var scope = services.BuildServiceProvider().CreateScope();
-            var context = CreateDbContext(scope, null);
+            var context = CreateDbContext(scope, "test");
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
+            
+            // seed with test keys
+            // hash of secret
+            context.ApiKeys.Add(new ApiKeyDesc() { Tenant = "test", Id = "9795", ApiKey = "test:public:2e728aa5986f4ba8b073a5b28a939795" });
+            context.ApiKeys.Add(new ApiKeyDesc() { Tenant = "test", Id = "6d02", ApiKey = "4RtmMr0hVknaQAIhaRtPHw==:xR7bg3NVsC80a8GDDhH39g==",Scopes = new[] { "token_register", "token_verify" }, });
+            context.AccountInfo.Add(new AccountMetaInformation() { Tenant = "test", AcountName = "test" });
+            context.SaveChanges();
         });
 
         return base.CreateHost(builder);
