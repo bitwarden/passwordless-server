3994e6482390b8d51c6a0a0b4dd949244a09ca60
Anders Åberg <anders@andersaberg.com>
2023-05-15 13:44:15 +0200

    Fixed form bug

--- src/AdminConsole/Pages/Organization/Join.cshtml.cs
+++ src/AdminConsole/Pages/Organization/Join.cshtml.cs
@@ -66,7 +66,7 @@ public class Join : PageModel
             ModelState.AddModelError("bad-invite", "Invite is invalid or expired");
         }
         
-        if (!Form.AcceptsTermsAndPrivacy)
+        if (!form.AcceptsTermsAndPrivacy)
         {
             ModelState.AddModelError("AcceptsTermsAndPrivacy", "You must accept the terms and privacy policy to continue.");
         }
