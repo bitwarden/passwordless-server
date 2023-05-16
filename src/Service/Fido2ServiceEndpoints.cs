0751471c4e5488a05b3f8387d9469f3e38d03c98
Anders Åberg <anders@andersaberg.com>
2023-05-16 11:21:01 +0200

    Introduce Common UnknownCredentialException to improve reporting

--- src/Service/Fido2ServiceEndpoints.cs
+++ src/Service/Fido2ServiceEndpoints.cs
@@ -292,8 +292,8 @@ public class Fido2ServiceEndpoints
         var creds = await _storage.GetCredential(request.Response.Id);
         if (creds == null)
         {
-            throw new ApiException("unknown_credential", "We don't recognize the credentials you sent us.", 400,
-                new() { { "credentialId", request.Response.Id } });
+            throw new UnknownCredentialException("unknown_credential", "We don't recognize the passkey you sent us.", 400,
+                new() { { "credentialId", Base64Url.Encode(request.Response.Id) } });
         }
 
         // Get credential counter from database
---
+++ src/Service/Helpers/UnknownCredentialException.cs
@@ -0,0 +1,15 @@
+namespace Passwordless.Service.Helpers;
+
+public class UnknownCredentialException : ApiException {
+    public UnknownCredentialException(string message) : base(message)
+    {
+    }
+
+    public UnknownCredentialException(string message, int statusCode, Dictionary<string, object> extras = null) : base(message, statusCode, extras)
+    {
+    }
+
+    public UnknownCredentialException(string errorCode, string message, int statusCode, Dictionary<string, object> extras = null) : base(errorCode, message, statusCode, extras)
+    {
+    }
+}
