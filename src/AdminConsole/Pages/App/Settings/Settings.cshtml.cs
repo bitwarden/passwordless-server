7388c18b2b270fdf0d72ab1da37fab7abc19676f
Anders Åberg <anders@andersaberg.com>
2023-05-17 17:55:23 +0200

    Improved copy and ux (#26)
    * Improved copy and ux
    
    * Improved get started copy as well

--- src/AdminConsole/Pages/App/Settings/Settings.cshtml.cs
+++ src/AdminConsole/Pages/App/Settings/Settings.cshtml.cs
@@ -1,18 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using AdminConsole.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AdminConsole.Pages.Settings;
 
 public class SettingsModel : PageModel
 {
     private readonly ILogger<SettingsModel> _logger;
+    private readonly DataService _dataService;
 
-    public SettingsModel(ILogger<SettingsModel> logger)
+    public Models.Organization Organization { get; set; }
+
+    public SettingsModel(ILogger<SettingsModel> logger, DataService dataService)
     {
         _logger = logger;
+        _dataService = dataService;
     }
 
-    public void OnGet()
+    public async Task OnGet()
     {
-
+        Organization = await _dataService.GetOrganization();
     }
 }
