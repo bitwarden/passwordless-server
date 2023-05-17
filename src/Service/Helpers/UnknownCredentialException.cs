0d7a7077fb7af7f721ac111b2780c2f76c252b24
Anders Åberg <anders@andersaberg.com>
2023-05-17 09:49:04 +0200

    dotnet format

--- src/Service/Helpers/UnknownCredentialException.cs
+++ src/Service/Helpers/UnknownCredentialException.cs
@@ -3,7 +3,7 @@ namespace Passwordless.Service.Helpers;
 public class UnknownCredentialException : ApiException
 {
     public UnknownCredentialException(string credentialId)
-        : base("unknown_credential", "We don't recognize the passkey you sent us.", 400, new () { { "credentialId", credentialId }})
+        : base("unknown_credential", "We don't recognize the passkey you sent us.", 400, new() { { "credentialId", credentialId } })
     {
     }
 }
