7ca4ad7798adea811f6f8e84ffa52f909fe94849
Anders Åberg <anders@andersaberg.com>
2023-05-18 19:50:36 +0200

    even easier development (#30)
    * Added experimental support for doing migrations in the API project
    
    * dotnet format
    
    * Use Logger
    
    * Working migrations and seeded api keys
    
    Need cleanup

--- src/AdminConsole/Services/Mail/FileMailProvider.cs
+++ src/AdminConsole/Services/Mail/FileMailProvider.cs
@@ -3,12 +3,18 @@ namespace AdminConsole.Services.Mail;
 // ReSharper disable once UnusedType.Global
 public class FileMailProvider : IMailProvider
 {
+    private readonly ILogger<IMailProvider> _logger;
+
+    public FileMailProvider(ILogger<IMailProvider> logger)
+    {
+        _logger = logger;
+    }
     public Task SendAsync(MailMessage message)
     {
         string msg = message.TextBody;
-        Console.WriteLine("> Sent email to mail.md");
         msg = "# New message " + DateTime.Now + Environment.NewLine + Environment.NewLine + msg + Environment.NewLine + Environment.NewLine;
         File.AppendAllText("mail.md", msg);
+        _logger.LogInformation("Sent email to mail.md");
 
         return Task.CompletedTask;
     }
--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,10 +1,14 @@
 using System.Reflection;
 using System.Text.Json;
+using Microsoft.EntityFrameworkCore;
 using Passwordless.Api.Authorization;
 using Passwordless.Api.Endpoints;
 using Passwordless.Api.Helpers;
 using Passwordless.Server.Endpoints;
 using Passwordless.Service;
+using Passwordless.Service.Models;
+using Passwordless.Service.Storage;
+using Passwordless.Service.Storage.Ef;
 using Serilog;
 using Serilog.Sinks.Datadog.Logs;
 
@@ -75,11 +79,17 @@ builder.Services.AddSingleton(sp =>
     sp.GetRequiredService<ILoggerFactory>().CreateLogger("NonTyped"));
 
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+}
+
 WebApplication app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
 }
 else
 {
@@ -93,9 +103,34 @@ app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet("/",
-    () =>
-        "Hey, this place is for computers. Check out our human documentation instead: https://docs.passwordless.dev");
+if (app.Environment.IsDevelopment())
+{
+    app.Map("/", async (DbTenantContext dbTenantContext ) =>
+    {
+        // TODO: If people complain, put this behind ?do=migrate and just return a link here.
+        await dbTenantContext.Database.MigrateAsync();
+        
+        // seed with a development api key
+        if (!await dbTenantContext.ApiKeys.AnyAsync())
+        {
+            dbTenantContext.ApiKeys.Add(new ApiKeyDesc() { Tenant = "test", Id = "9795", ApiKey = "test:public:2e728aa5986f4ba8b073a5b28a939795" });
+            dbTenantContext.ApiKeys.Add(new ApiKeyDesc() { Tenant = "test", Id = "6d02", ApiKey = "4RtmMr0hVknaQAIhaRtPHw==:xR7bg3NVsC80a8GDDhH39g==", Scopes = new[] { "token_register", "token_verify" }, });
+            dbTenantContext.AccountInfo.Add(new AccountMetaInformation() { Tenant = "test", AcountName = "test" });
+            await dbTenantContext.SaveChangesAsync();
+
+            return "Database created and seeded.";
+        }
+        
+        return "All OK. Happy Developing!";
+    });
+}
+else
+{
+    app.MapGet("/",
+        () =>
+            "Hey, this place is for computers. Check out our human documentation instead: https://docs.passwordless.dev");    
+}
+
 app.UseMiddleware<LoggingMiddleware>();
 app.UseSerilogRequestLogging();
 app.UseMiddleware<FriendlyExceptionsMiddleware>();
