d0ca91f140b4afe25dd8a28f826b09b6ed005539
Anders Åberg <anders@andersaberg.com>
2023-06-30 10:26:06 +0200

    format

--- src/Service/Fido2ServiceEndpoints.cs
+++ src/Service/Fido2ServiceEndpoints.cs
@@ -3,7 +3,6 @@ using System.Security.Cryptography;
 using System.Text;
 using Fido2NetLib;
 using Fido2NetLib.Objects;
-using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
