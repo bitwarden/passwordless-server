a432731a73b21b797746d4c3e25143d2b8846780
Jonas Hendrickx <hendrickx.jonas.pvt@gmail.com>
2023-07-28 12:13:33 +0200

    Remove redundant properties in 'CredentialsDeleteDTO' and 'CredentialsListDTO' (#52)
    Co-authored-by: Anders Åberg <anders@andersaberg.com>

--- src/Api/Models/CredentialsDeleteDTO.cs
+++ src/Api/Models/CredentialsDeleteDTO.cs
@@ -5,8 +5,6 @@ namespace Passwordless.Api.Models;
 
 public class CredentialsDeleteDTO
 {
-    public string ApiSecret { get; set; } = "";
-
     // TODO: Add Base64Url converter
     [JsonConverter(typeof(Base64UrlConverter))]
     public byte[] CredentialId { get; set; } = Array.Empty<byte>();
