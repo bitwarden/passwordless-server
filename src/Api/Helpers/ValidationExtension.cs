46403afb8dd46c32f58e46c3540b4c0ee4b69ed4
jrmccannon <jared.r.mccannon@gmail.com>
2023-08-01 08:41:12 -0500

    Formatting issues.

--- src/Api/Helpers/ValidationExtension.cs
+++ src/Api/Helpers/ValidationExtension.cs
@@ -6,5 +6,6 @@ namespace Passwordless.Api.Helpers;
 
 public static class ValidationExtension
 {
-    public static IServiceCollection AddValidators(this IServiceCollection services) => services.AddTransient<IValidator<RegistrationCompleteDTO>, RegistrationCompleteDTOValidator>();
+    public static IServiceCollection AddValidators(this IServiceCollection services)
+        => services.AddTransient<IValidator<RegistrationCompleteDTO>, RegistrationCompleteDTOValidator>();
 }
