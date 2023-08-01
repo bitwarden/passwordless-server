b4efe9746d276de6e292f97eb97b7aef5d13f741
jrmccannon <jared.r.mccannon@gmail.com>
2023-08-01 08:50:01 -0500

    Renamed to ValidationExtensions.cs

---
+++ src/Api/Helpers/ValidationExtensions.cs
@@ -0,0 +1,11 @@
+using FluentValidation;
+using Passwordless.Api.Validators;
+using Passwordless.Service.Models;
+
+namespace Passwordless.Api.Helpers;
+
+public static class ValidationExtensions
+{
+    public static IServiceCollection AddValidators(this IServiceCollection services) =>
+        services.AddTransient<IValidator<RegistrationCompleteDTO>, RegistrationCompleteDTOValidator>();
+}
