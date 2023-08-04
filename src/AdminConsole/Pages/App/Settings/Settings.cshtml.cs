d6db34571211dc67fb365ee29b949aeb0cc9092d
jrmccannon <jared.r.mccannon@gmail.com>
2023-08-04 08:33:01 -0500

    Passed ApplicationId from current context.

--- src/AdminConsole/Pages/App/Settings/Settings.cshtml.cs
+++ src/AdminConsole/Pages/App/Settings/Settings.cshtml.cs
@@ -7,17 +7,21 @@ public class SettingsModel : PageModel
 {
     private readonly ILogger<SettingsModel> _logger;
     private readonly DataService _dataService;
+    private readonly ICurrentContext _currentContext;
 
     public Models.Organization Organization { get; set; }
+    public string ApplicationId { get; set; }
 
-    public SettingsModel(ILogger<SettingsModel> logger, DataService dataService)
+    public SettingsModel(ILogger<SettingsModel> logger, DataService dataService, ICurrentContext currentContext)
     {
         _logger = logger;
         _dataService = dataService;
+        _currentContext = currentContext;
     }
 
     public async Task OnGet()
     {
         Organization = await _dataService.GetOrganization();
+        ApplicationId = _currentContext.AppId ?? String.Empty;
     }
 }
