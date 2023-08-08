81897ada93078ad8ca254c7f8bade04c7c31700c
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-08-08 12:49:46 +0200

    Use published .NET SDK from now on. (#64)
    * Use published .NET SDK from now on.
    
    * Use published .NET SDK from now on.

--- src/Sdk/PasswordlessExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Passwordless.Net;
-
-public static class PasswordlessExtensions
-{
-    public static string ToBase64Url(this byte[] bytes)
-    {
-        return Base64Url.Encode(bytes);
-    }
-}
