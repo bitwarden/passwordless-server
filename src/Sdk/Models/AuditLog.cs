e4808c76f4789edbb0161335f546ebfe8c37b6fb
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-08-08 11:28:26 +0200

    Use published .NET SDK from now on.

--- src/Sdk/Models/AuditLog.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Passwordless.Net;
-
-public class AuditLog
-{
-    public DateTime Timestamp { get; set; }
-    public string Level { get; set; }
-    public string Message { get; set; }
-    public string Details { get; set; }
-}
