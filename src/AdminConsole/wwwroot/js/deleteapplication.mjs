67853051a7eb01a84bfca2e063ea0b1370679432
Anders Åberg <anders@andersaberg.com>
2023-08-10 11:01:55 +0200

    Tweaked Settings page

--- src/AdminConsole/wwwroot/js/deleteapplication.mjs
+++ src/AdminConsole/wwwroot/js/deleteapplication.mjs
@@ -1,12 +1,14 @@
-import { ref, computed } from "vue";
+import {ref, computed} from "vue";
 
 export default {
     template: /*html*/`
     <div class="mt-3">
-        <button id="btn-delete-app" class="btn-danger disabled:opacity-25" :disabled="disableDelete" @click="enableConfirmation">Delete Application</button>
+        <button v-if="!showConfirmation" id="btn-delete-app" class="btn-secondary disabled:opacity-25" :disabled="disableDelete" @click="enableConfirmation">Delete Application...</button>
         <template v-if="showConfirmation" class="flex flex-col">
-            <input required placeholder="Name of application" class="ml-3 rounded-md border bg-white shadow-sm focus:outline-none" v-model="confirmInputValue"/>
-            <button id="btn-confirm-delete" class="ml-3 btn-primary disabled:opacity-25" type="submit" :disabled="disableConfirmation">I'm Sure</button>
+            <div class="my-2 flex rounded-md shadow-sm">
+                <input required placeholder="Name of application" type="text" v-model="confirmInputValue" class="block font-mono rounded-none rounded-l-md border-0 py-1.5 pl-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                <button title="I'm sure, delete the applications" class="btn-danger rounded-l-none disabled:opacity-25" type="submit" :disabled="disableConfirmation">I'm Sure</button>
+            </div>                        
             <p class="italic mt-3">Type name of application and click "I'm Sure"</p>
         </template>
     </div>
@@ -16,16 +18,16 @@ export default {
         const disableDelete = ref(false);
         const showConfirmation = ref(false);
         const confirmInputValue = ref('');
-        
+
         const disableConfirmation = computed(() => {
             return props.appId !== confirmInputValue.value;
         });
-        
+
         const enableConfirmation = function () {
             showConfirmation.value = true;
             disableDelete.value = true;
-        } 
-        
+        }
+
         return {
             disableDelete,
             showConfirmation,
