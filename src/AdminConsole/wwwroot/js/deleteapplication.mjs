fa5e83f83f38526b9f48b3d466f0515e7ffd29da
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-08-17 11:30:06 +0200

    PAS147: Delete Application - Fix translations

--- src/AdminConsole/wwwroot/js/deleteapplication.mjs
+++ src/AdminConsole/wwwroot/js/deleteapplication.mjs
@@ -3,7 +3,7 @@ import {ref, computed} from "vue";
 export default {
     template: /*html*/`
     <div class="mt-3">
-        <button type="button" v-if="!showConfirmation" id="btn-delete-app" class="btn-secondary disabled:opacity-25" :disabled="disableDelete" @click="enableConfirmation">Delete Application...</button>
+        <button type="button" v-if="!showConfirmation" id="btn-delete-app" class="btn-secondary disabled:opacity-25" :disabled="disableDelete" @click="enableConfirmation">Delete Application</button>
         <template v-if="showConfirmation" class="flex flex-col">
             <div class="my-2 flex rounded-md shadow-sm">
                 <input required placeholder="Name of application" type="text" v-model="confirmInputValue" class="block font-mono rounded-none rounded-l-md border-0 py-1.5 pl-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
