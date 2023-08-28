aba7c9836526cd3851232851c656929d226bc8e5
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-08-28 17:08:14 +0200

    Reference Passwordless ASP.NET SDK v0.0.13 & Passwordless .NET SDK v0.0.14

--- src/AspNetCore/Services/Implementations/NoopCustomizeRegisterOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace Passwordless.AspNetCore.Services.Implementations;
-
-internal sealed class NoopCustomizeRegisterOptions : ICustomizeRegisterOptions
-{
-    public Task CustomizeAsync(CustomizeRegisterOptionsContext context, CancellationToken cancellationToken)
-    {
-        return Task.CompletedTask;
-    }
-}
