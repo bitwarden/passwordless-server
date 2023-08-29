20e003a364c0f0eab1965046448747e4328a28e3
jrmccannon <jared.r.mccannon@gmail.com>
2023-08-29 16:12:37 -0500

    formatting changes.

--- src/AdminConsole/Pages/Components/PagedList.cs
+++ src/AdminConsole/Pages/Components/PagedList.cs
@@ -4,12 +4,12 @@ public class PagedList
 {
     public int CurrentIndex { get; set; }
     public int TotalPages { get; set; }
-    
+
     public IEnumerable<int> Pages { get; }
 
     public PagedList(int count, int pageIndex, int numberOfRecords)
     {
-        TotalPages = (int)Math.Ceiling(count/(double)numberOfRecords);
+        TotalPages = (int)Math.Ceiling(count / (double)numberOfRecords);
         CurrentIndex = pageIndex;
 
         Pages = Enumerable.Range(1, TotalPages);
--- src/AdminConsole/Pages/Organization/Log.cshtml.cs
+++ src/AdminConsole/Pages/Organization/Log.cshtml.cs
@@ -1,7 +1,6 @@
 using AdminConsole.Pages.Components;
 using AdminConsole.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Passwordless.AdminConsole.AuditLog.DTOs;
 using Passwordless.AdminConsole.Services;
 
@@ -16,7 +15,7 @@ public class Log : PageModel
 
     public IEnumerable<AuditLogEvent> Events { get; set; }
     public PagedList PageList { get; set; }
-    
+
     public Log(IAuditLogService auditLogService, DataService dataService)
     {
         _auditLogService = auditLogService;
@@ -29,7 +28,7 @@ public class Log : PageModel
         Events = (await _auditLogService.GetAuditLogs(Organization.Id, pageNumber, numberOfResults)).Events;
 
         var itemCount = await _auditLogService.GetAuditLogCount(Organization.Id);
-        
+
         PageList = new PagedList(itemCount, pageNumber, numberOfResults);
     }
 }
