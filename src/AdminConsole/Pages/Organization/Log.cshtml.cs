04dfae106965667540aa44f91d6857d820654344
jrmccannon <jared.r.mccannon@gmail.com>
2023-09-13 11:15:09 -0500

    Added empty state. Added redirect if feature is not enabled. Both for org logs.

--- src/AdminConsole/Pages/Organization/Log.cshtml.cs
+++ src/AdminConsole/Pages/Organization/Log.cshtml.cs
@@ -1,6 +1,8 @@
 using AdminConsole.Pages.Components;
 using AdminConsole.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Passwordless.AdminConsole;
 using Passwordless.AdminConsole.AuditLog.DTOs;
 using Passwordless.AdminConsole.Services;
 
@@ -10,24 +12,34 @@ public class Log : PageModel
 {
     private readonly IAuditLogService _auditLogService;
     private readonly DataService _dataService;
+    private readonly ICurrentContext _currentContext;
 
     public Models.Organization Organization { get; set; }
     public IEnumerable<AuditLogEvent> Events { get; set; }
     public PagedList PageList { get; set; }
+    public int RetentionPeriod { get; private set; }
 
-    public Log(IAuditLogService auditLogService, DataService dataService)
+    public Log(IAuditLogService auditLogService, DataService dataService, ICurrentContext currentContext)
     {
         _auditLogService = auditLogService;
         _dataService = dataService;
+        _currentContext = currentContext;
     }
 
-    public async Task OnGet(int pageNumber = 1, int numberOfResults = 100)
+    public async Task<ActionResult> OnGet(int pageNumber = 1, int numberOfResults = 100)
     {
+        var features = _currentContext.OrganizationFeatures;
+
+        if (features.AuditLoggingIsEnabled) return RedirectToPage("Overview");
+
+        RetentionPeriod = features.AuditLoggingRetentionPeriod;
         Organization = await _dataService.GetOrganization();
         Events = (await _auditLogService.GetAuditLogs(Organization.Id, pageNumber, numberOfResults)).Events;
 
         var itemCount = await _auditLogService.GetAuditLogCount(Organization.Id);
 
         PageList = new PagedList(itemCount, pageNumber, numberOfResults);
+
+        return Page();
     }
 }
