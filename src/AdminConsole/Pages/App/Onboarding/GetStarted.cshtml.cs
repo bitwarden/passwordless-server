7984c5b7354c63d26a5b104e117d1788795358a5
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2023-09-13 12:37:27 +0000

    Show correct API url when running locally

--- src/AdminConsole/Pages/App/Onboarding/GetStarted.cshtml.cs
+++ src/AdminConsole/Pages/App/Onboarding/GetStarted.cshtml.cs
@@ -2,7 +2,9 @@ using AdminConsole.Db;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Passwordless.AdminConsole;
+using Passwordless.Net;
 
 namespace AdminConsole.Pages.App.Onboarding;
 
@@ -10,13 +12,17 @@ public class GetStarted : PageModel
 {
     private readonly ConsoleDbContext _dbContext;
     private readonly ICurrentContext _context;
+    private readonly IOptionsSnapshot<PasswordlessOptions> _passwordlessOptions;
+
+    public string ApiUrl => _passwordlessOptions.Value.ApiUrl;
 
     public Models.Onboarding Onboarding { get; set; }
 
-    public GetStarted(ConsoleDbContext dbContext, ICurrentContext context)
+    public GetStarted(ConsoleDbContext dbContext, ICurrentContext context, IOptionsSnapshot<PasswordlessOptions> passwordlessOptions)
     {
         _dbContext = dbContext;
         _context = context;
+        _passwordlessOptions = passwordlessOptions;
     }
 
     public async Task<IActionResult> OnGet()
