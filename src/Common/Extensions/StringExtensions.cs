f8abaa8fefcab8d43ad0e713162c0b4c6babf445
jrmccannon <jared.r.mccannon@gmail.com>
2023-09-15 16:04:33 -0500

    Added unit tests.

--- src/Common/Extensions/StringExtensions.cs
+++ src/Common/Extensions/StringExtensions.cs
@@ -2,8 +2,8 @@ namespace Passwordless.Common.Extensions;
 
 public static class StringExtensions
 {
-    public static string GetLast(this string input, int charactersToReturn) =>
-        charactersToReturn >= input.Length
+    public static string GetLast(this string input, int characterCountToReturn) =>
+        string.IsNullOrWhiteSpace(input) || characterCountToReturn >= input.Length
             ? input
-            : input[^charactersToReturn..];
+            : input[^characterCountToReturn..];
 }
