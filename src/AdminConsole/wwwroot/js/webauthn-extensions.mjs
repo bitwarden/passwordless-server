d8a6b1ebc78a2c582593a2c0615a38a6a19f5cf0
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-09-18 07:34:15 +0200

    Unused

--- src/AdminConsole/wwwroot/js/webauthn-extensions.mjs
+++
@@ -1,9 +0,0 @@
-export function disableHtmlElementIfWebAuthnUnsupported(elementId) {
-    const isSupported = Passwordless.isBrowserSupported();
-    if (!isSupported) {
-        const element = document.getElementById(elementId);
-        if (element) {
-            element.disabled = true;
-        }
-    }
-}
