f76f8d57f815883ac7ffff01fbe147ae17c2e7db
jrmccannon <jared.r.mccannon@gmail.com>
2023-09-22 10:46:09 -0500

    Added OrgEvent table to migrations.

--- src/AdminConsole/Migrations/Mssql/20230922153632_AddOrgEventTable.Designer.cs
+++ src/AdminConsole/Migrations/Mssql/20230922153632_AddOrgEventTable.Designer.cs
@@ -4,6 +4,7 @@ using AdminConsole.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Passwordless.AdminConsole.Migrations.Mssql
 {
     [DbContext(typeof(MssqlConsoleDbContext))]
-    partial class MssqlConsoleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230922153632_AddOrgEventTable")]
+    partial class AddOrgEventTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
