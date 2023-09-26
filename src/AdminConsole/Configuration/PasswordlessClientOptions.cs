ebd39ba999b4cf87d989ed6c5b945d2b851b898f
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-09-26 08:11:56 +0200

    Refactoring & cleanup of options pattern.

--- src/AdminConsole/Configuration/PasswordlessClientOptions.cs
+++
@@ -1,9 +0,0 @@
-using Passwordless.Net;
-
-namespace Passwordless.AdminConsole;
-
-public class PasswordlessClientOptions
-{
-    public string ApiKey { get; set; } = null!;
-    public string ApiUrl { get; set; } = PasswordlessOptions.CloudApiUrl;
-}
