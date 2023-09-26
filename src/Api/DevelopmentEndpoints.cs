82a2be5f9a6dc5b8c935a8e6d5a03508e3603b18
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-09-26 15:09:09 +0200

    revert

--- src/Api/DevelopmentEndpoints.cs
+++ src/Api/DevelopmentEndpoints.cs
@@ -39,8 +39,5 @@ public static class DevelopmentEndpoints
 
             return "All OK. Happy Developing!";
         });
-        
-        app.MapGet("health/throw/api", (ctx) => throw new ApiException("test_error", "Testing error response", 400));
-        app.MapGet("health/throw/exception", (ctx) => throw new Exception("Testing error response", new Exception("Inner exception")));
     }
 }
---
+++ src/Api/Endpoints/Health.cs
@@ -0,0 +1,12 @@
+using Passwordless.Service.Helpers;
+
+namespace Passwordless.Server.Endpoints;
+
+public static class HealthEndpoints
+{
+    public static void MapHealthEndpoints(this WebApplication app)
+    {
+        app.MapGet("health/throw/api", (ctx) => throw new ApiException("test_error", "Testing error response", 400));
+        app.MapGet("health/throw/exception", (ctx) => throw new Exception("Testing error response", new Exception("Inner exception")));
+    }
+}
