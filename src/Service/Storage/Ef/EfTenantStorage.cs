e8fba1a4520ad90ddb457dfb000ddad53604afb5
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-09-28 12:03:48 +0200

    Got it working again. Temporarily commit.

--- src/Service/Storage/Ef/EfTenantStorage.cs
+++ src/Service/Storage/Ef/EfTenantStorage.cs
@@ -104,7 +104,7 @@ public class EfTenantStorage : ITenantStorage
         // Do we really need these AsNoTracking?
         var descs = await db.Credentials.Where(c => c.UserId == userid).ToListAsync();
 
-        var pkcred = descs.Select(x => new PublicKeyCredentialDescriptor() { Id = x.DescriptorId, Transports = x.DescriptorTransports, Type = x.DescriptorType }).ToList();
+        var pkcred = descs.Select(x => new PublicKeyCredentialDescriptor(x.DescriptorType.Value, x.DescriptorId, x.DescriptorTransports)).ToList();
         return pkcred;
     }
 
@@ -113,7 +113,7 @@ public class EfTenantStorage : ITenantStorage
         // TODO: Returning a c.descriptor does not work.
         var res = await db.Credentials.Where(c => c.UserId == userId).ToListAsync();
 
-        var pkcred = res.Select(x => new PublicKeyCredentialDescriptor() { Id = x.DescriptorId, Transports = x.DescriptorTransports, Type = x.DescriptorType }).ToList();
+        var pkcred = res.Select(x => new PublicKeyCredentialDescriptor(x.DescriptorType.Value, x.DescriptorId, x.DescriptorTransports)).ToList();
 
         return pkcred;
     }
