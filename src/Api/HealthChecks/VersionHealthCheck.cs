dc16b77d82c1c1d8e14044f4bce5f7a0aec2d9a9
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2023-10-30 15:34:03 +0200

    Version checks (#165)
    * Fetch informational version in version health checks
    
    * Fix API version check
    
    * Clean up
    
    * dotnet format
    
    ---------
    
    Co-authored-by: Anders Åberg <anders@andersaberg.com>

--- src/Api/HealthChecks/VersionHealthCheck.cs
+++ src/Api/HealthChecks/VersionHealthCheck.cs
@@ -1,19 +1,22 @@
 using System.Reflection;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Passwordless.Common.Extensions;
 
 namespace Passwordless.Api.HealthChecks;
 
 public class VersionHealthCheck : IHealthCheck
 {
-    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
     {
-        var version = Assembly.GetExecutingAssembly().GetName().Version;
-        return HealthCheckResult.Healthy(version.ToString(), new Dictionary<string, object>
-        {
-            {nameof(version.Major), version.Major},
-            {nameof(version.Minor), version.Minor},
-            {nameof(version.Revision), version.Revision},
-            {nameof(version.Build), version.Build},
-        });
+        var assembly = Assembly.GetExecutingAssembly();
+
+        return Task.FromResult(
+            HealthCheckResult.Healthy(data: new Dictionary<string, object>
+            {
+                ["version"] = assembly.GetInformationalVersion() ??
+                              assembly.GetName().Version?.ToString() ??
+                              "unknown version"
+            })
+        );
     }
 }
