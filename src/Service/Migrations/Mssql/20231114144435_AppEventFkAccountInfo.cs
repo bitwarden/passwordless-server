06bdf5b16acc94fac77c1e75b357dfaccf98123b
jrmccannon <jared.r.mccannon@gmail.com>
2023-11-14 09:24:23 -0600

    formatting

--- src/Service/Migrations/Mssql/20231114144435_AppEventFkAccountInfo.cs
+++ src/Service/Migrations/Mssql/20231114144435_AppEventFkAccountInfo.cs
@@ -2,56 +2,55 @@
 
 #nullable disable
 
-namespace Passwordless.Service.Migrations.Mssql
+namespace Passwordless.Service.Migrations.Mssql;
+
+/// <inheritdoc />
+public partial class AppEventFkAccountInfo : Migration
 {
     /// <inheritdoc />
-    public partial class AppEventFkAccountInfo : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "TenantId",
-                table: "ApplicationEvents",
-                type: "nvarchar(450)",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "nvarchar(max)",
-                oldNullable: true);
+        migrationBuilder.AlterColumn<string>(
+            name: "TenantId",
+            table: "ApplicationEvents",
+            type: "nvarchar(450)",
+            nullable: false,
+            defaultValue: "",
+            oldClrType: typeof(string),
+            oldType: "nvarchar(max)",
+            oldNullable: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_ApplicationEvents_TenantId",
-                table: "ApplicationEvents",
-                column: "TenantId");
+        migrationBuilder.CreateIndex(
+            name: "IX_ApplicationEvents_TenantId",
+            table: "ApplicationEvents",
+            column: "TenantId");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_ApplicationEvents_AccountInfo_TenantId",
-                table: "ApplicationEvents",
-                column: "TenantId",
-                principalTable: "AccountInfo",
-                principalColumn: "AcountName",
-                onDelete: ReferentialAction.Cascade);
-        }
+        migrationBuilder.AddForeignKey(
+            name: "FK_ApplicationEvents_AccountInfo_TenantId",
+            table: "ApplicationEvents",
+            column: "TenantId",
+            principalTable: "AccountInfo",
+            principalColumn: "AcountName",
+            onDelete: ReferentialAction.Cascade);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "FK_ApplicationEvents_AccountInfo_TenantId",
-                table: "ApplicationEvents");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropForeignKey(
+            name: "FK_ApplicationEvents_AccountInfo_TenantId",
+            table: "ApplicationEvents");
 
-            migrationBuilder.DropIndex(
-                name: "IX_ApplicationEvents_TenantId",
-                table: "ApplicationEvents");
+        migrationBuilder.DropIndex(
+            name: "IX_ApplicationEvents_TenantId",
+            table: "ApplicationEvents");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "TenantId",
-                table: "ApplicationEvents",
-                type: "nvarchar(max)",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "nvarchar(450)");
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "TenantId",
+            table: "ApplicationEvents",
+            type: "nvarchar(max)",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "nvarchar(450)");
     }
 }
--- src/Service/Migrations/Sqlite/20231113215305_AppEventFkAccountInfo.cs
+++ src/Service/Migrations/Sqlite/20231113215305_AppEventFkAccountInfo.cs
@@ -2,56 +2,55 @@
 
 #nullable disable
 
-namespace Passwordless.Service.Migrations.Sqlite
+namespace Passwordless.Service.Migrations.Sqlite;
+
+/// <inheritdoc />
+public partial class AppEventFkAccountInfo : Migration
 {
     /// <inheritdoc />
-    public partial class AppEventFkAccountInfo : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterColumn<string>(
-                name: "TenantId",
-                table: "ApplicationEvents",
-                type: "TEXT",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "TEXT",
-                oldNullable: true);
+        migrationBuilder.AlterColumn<string>(
+            name: "TenantId",
+            table: "ApplicationEvents",
+            type: "TEXT",
+            nullable: false,
+            defaultValue: "",
+            oldClrType: typeof(string),
+            oldType: "TEXT",
+            oldNullable: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_ApplicationEvents_TenantId",
-                table: "ApplicationEvents",
-                column: "TenantId");
+        migrationBuilder.CreateIndex(
+            name: "IX_ApplicationEvents_TenantId",
+            table: "ApplicationEvents",
+            column: "TenantId");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_ApplicationEvents_AccountInfo_TenantId",
-                table: "ApplicationEvents",
-                column: "TenantId",
-                principalTable: "AccountInfo",
-                principalColumn: "AcountName",
-                onDelete: ReferentialAction.Cascade);
-        }
+        migrationBuilder.AddForeignKey(
+            name: "FK_ApplicationEvents_AccountInfo_TenantId",
+            table: "ApplicationEvents",
+            column: "TenantId",
+            principalTable: "AccountInfo",
+            principalColumn: "AcountName",
+            onDelete: ReferentialAction.Cascade);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "FK_ApplicationEvents_AccountInfo_TenantId",
-                table: "ApplicationEvents");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropForeignKey(
+            name: "FK_ApplicationEvents_AccountInfo_TenantId",
+            table: "ApplicationEvents");
 
-            migrationBuilder.DropIndex(
-                name: "IX_ApplicationEvents_TenantId",
-                table: "ApplicationEvents");
+        migrationBuilder.DropIndex(
+            name: "IX_ApplicationEvents_TenantId",
+            table: "ApplicationEvents");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "TenantId",
-                table: "ApplicationEvents",
-                type: "TEXT",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "TEXT");
-        }
+        migrationBuilder.AlterColumn<string>(
+            name: "TenantId",
+            table: "ApplicationEvents",
+            type: "TEXT",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "TEXT");
     }
 }
