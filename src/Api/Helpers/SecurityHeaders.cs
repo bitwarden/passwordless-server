26225608c78c0ba634500fcc9f28223eb42ff51c
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-11-16 13:27:33 +0100

    .NET 8 migration (#205)

--- src/Api/Helpers/SecurityHeaders.cs
+++ src/Api/Helpers/SecurityHeaders.cs
@@ -7,9 +7,9 @@ public static class SecurityHeaders
         app.Use((context, next) =>
         {
             const string csp = "default-src 'self';";
-            context.Response.Headers.Add("Content-Security-Policy", new[] { csp });
-            context.Response.Headers.Add("X-Content-Type-Options", new[] { "nosniff" });
-            context.Response.Headers.Add("Referrer-Policy", new[] { "no-referrer" });
+            context.Response.Headers.Append("Content-Security-Policy", new[] { csp });
+            context.Response.Headers.Append("X-Content-Type-Options", new[] { "nosniff" });
+            context.Response.Headers.Append("Referrer-Policy", new[] { "no-referrer" });
             return next();
         });
 
