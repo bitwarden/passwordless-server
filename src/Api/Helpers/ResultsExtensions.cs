22d0499c5a8658afdd28f9109b97f3fd0942fe40
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-11-21 10:17:07 +0100

    Remove dead code (#231)

--- src/Api/Helpers/ResultsExtensions.cs
+++
@@ -1,9 +0,0 @@
-namespace Passwordless.Api.Helpers;
-
-// public static class ResultsExtensions
-// {
-//     public static IResult OkBase<T>(T value) where T : ResponseBase
-//     {
-//         return Ok(value);
-//     }
-// }
