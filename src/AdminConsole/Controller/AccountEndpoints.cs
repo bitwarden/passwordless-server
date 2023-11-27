1912b90c8e50657362bfb3f8b73c4fbc0070a24b
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-11-27 15:11:32 +0100

    add doc

--- src/AdminConsole/Controller/AccountEndpoints.cs
+++ src/AdminConsole/Controller/AccountEndpoints.cs
@@ -8,6 +8,8 @@ public static class AccountEndpoints
     public static IEndpointRouteBuilder MapAccountEndpoints(this IEndpointRouteBuilder endpoints)
     {
         var group = endpoints.MapGroup("/Account");
+        
+        // The Blazor SSR sample uses the same solution for signing out, but we do not want to use all the endpoints.
         group.MapPost("/Logout", async (SignInManager<ConsoleAdmin> signInManager) =>
         {
             await signInManager.SignOutAsync();
