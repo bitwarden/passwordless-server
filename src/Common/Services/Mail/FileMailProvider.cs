03410b9f0f5b86cd099ca8f2d9edb9e79a68b8d4
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-12-06 15:19:43 +0100

    Have FileMailProvider write to the mounted volume in a self-hosting environment.

--- src/Common/Services/Mail/FileMailProvider.cs
+++ src/Common/Services/Mail/FileMailProvider.cs
@@ -1,20 +1,29 @@
+using Microsoft.Extensions.Options;
+
 namespace Passwordless.Common.Services.Mail;
 
 // ReSharper disable once UnusedType.Global
 public class FileMailProvider : IMailProvider
 {
+    private readonly string _path;
+    private readonly TimeProvider _timeProvider;
     private readonly ILogger<IMailProvider> _logger;
 
-    public FileMailProvider(ILogger<IMailProvider> logger)
+    public FileMailProvider(
+        TimeProvider timeProvider,
+        IOptions<FileMailProviderConfiguration> configuration,
+        ILogger<IMailProvider> logger)
     {
+        _timeProvider = timeProvider;
+        _path = string.IsNullOrEmpty(configuration.Value.Path) ? "mail.md" : $"{configuration.Value.Path}/mail.md";
         _logger = logger;
     }
 
     public Task SendAsync(MailMessage message)
     {
         string msg = message.TextBody;
-        msg = "# New message " + DateTime.Now + Environment.NewLine + Environment.NewLine + msg + Environment.NewLine + Environment.NewLine;
-        File.AppendAllText("mail.md", msg);
+        msg = "# New message " + _timeProvider.GetLocalNow() + Environment.NewLine + Environment.NewLine + msg + Environment.NewLine + Environment.NewLine;
+        File.AppendAllText(_path, msg);
         _logger.LogInformation("Sent email to mail.md");
         return Task.CompletedTask;
     }
