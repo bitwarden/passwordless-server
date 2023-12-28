1afe8954d1f46fd8e453729ba51f46012496666a
Jonas Hendrickx <jhendrickx@bitwarden.com>
2023-12-28 15:16:52 +0100

    Load ApexCharts from NPM

---
+++ src/AdminConsole/post-build.sh
@@ -0,0 +1,4 @@
+mkdir wwwroot/lib/apexcharts
+cp node_modules/apexcharts/dist/apexcharts.min.js wwwroot/lib/apexcharts/
+cp node_modules/apexcharts/dist/apexcharts.css wwwroot/lib/apexcharts/
+cp -r node_modules/apexcharts/dist/locale wwwroot/lib/apexcharts/
