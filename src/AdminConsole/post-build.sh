57d5e0e1ad50cbb5c83bd1a4a0c7b684c2657e1c
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-01-01 09:35:12 +0100

    Not used

--- src/AdminConsole/post-build.sh
+++
@@ -1,4 +0,0 @@
-mkdir wwwroot/lib/apexcharts
-cp node_modules/apexcharts/dist/apexcharts.min.js wwwroot/lib/apexcharts/
-cp node_modules/apexcharts/dist/apexcharts.css wwwroot/lib/apexcharts/
-cp -r node_modules/apexcharts/dist/locale wwwroot/lib/apexcharts/
