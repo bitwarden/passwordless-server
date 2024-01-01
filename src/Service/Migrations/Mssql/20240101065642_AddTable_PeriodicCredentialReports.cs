9a36f729ade655335e3228b7bb9d8362af15ca50
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-01-01 07:57:32 +0100

    mssql migration

---
+++ src/Service/Migrations/Mssql/20240101065642_AddTable_PeriodicCredentialReports.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Passwordless.Service.Migrations.Mssql;
+
+/// <inheritdoc />
+public partial class AddTable_PeriodicCredentialReports : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "PeriodicCredentialReports",
+            columns: table => new
+            {
+                Tenant = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                Credentials = table.Column<int>(type: "int", nullable: false),
+                Users = table.Column<int>(type: "int", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_PeriodicCredentialReports", x => new { x.Tenant, x.CreatedAt });
+                table.ForeignKey(
+                    name: "FK_PeriodicCredentialReports_AccountInfo_Tenant",
+                    column: x => x.Tenant,
+                    principalTable: "AccountInfo",
+                    principalColumn: "AcountName",
+                    onDelete: ReferentialAction.Cascade);
+            });
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "PeriodicCredentialReports");
+    }
+}
