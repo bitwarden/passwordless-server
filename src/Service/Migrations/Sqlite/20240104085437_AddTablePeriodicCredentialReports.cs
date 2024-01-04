2ce4d13411eb07335671c5c4b8b4d6fc5e85141c
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-01-04 09:58:27 +0100

    format

--- src/Service/Migrations/Sqlite/20240104085437_AddTablePeriodicCredentialReports.cs
+++ src/Service/Migrations/Sqlite/20240104085437_AddTablePeriodicCredentialReports.cs
@@ -1,111 +1,109 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace Passwordless.Service.Migrations.Sqlite
+namespace Passwordless.Service.Migrations.Sqlite;
+
+/// <inheritdoc />
+public partial class AddTablePeriodicCredentialReports : Migration
 {
     /// <inheritdoc />
-    public partial class AddTablePeriodicCredentialReports : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "FK_PeriodicCredentialReport_AccountInfo_Tenant",
-                table: "PeriodicCredentialReport");
+        migrationBuilder.DropForeignKey(
+            name: "FK_PeriodicCredentialReport_AccountInfo_Tenant",
+            table: "PeriodicCredentialReport");
 
-            migrationBuilder.RenameTable(
-                name: "PeriodicCredentialReport",
-                newName: "PeriodicCredentialReports");
+        migrationBuilder.RenameTable(
+            name: "PeriodicCredentialReport",
+            newName: "PeriodicCredentialReports");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "Tenant",
-                table: "PeriodicCredentialReports",
-                type: "TEXT",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "TEXT",
-                oldNullable: true);
+        migrationBuilder.AlterColumn<string>(
+            name: "Tenant",
+            table: "PeriodicCredentialReports",
+            type: "TEXT",
+            nullable: false,
+            defaultValue: "",
+            oldClrType: typeof(string),
+            oldType: "TEXT",
+            oldNullable: true);
 
-            migrationBuilder.AddColumn<DateTime>(
-                name: "CreatedAt",
-                table: "PeriodicCredentialReports",
-                type: "TEXT",
-                nullable: false,
-                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        migrationBuilder.AddColumn<DateTime>(
+            name: "CreatedAt",
+            table: "PeriodicCredentialReports",
+            type: "TEXT",
+            nullable: false,
+            defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
 
-            migrationBuilder.AddColumn<int>(
-                name: "CredentialsCount",
-                table: "PeriodicCredentialReports",
-                type: "INTEGER",
-                nullable: false,
-                defaultValue: 0);
+        migrationBuilder.AddColumn<int>(
+            name: "CredentialsCount",
+            table: "PeriodicCredentialReports",
+            type: "INTEGER",
+            nullable: false,
+            defaultValue: 0);
 
-            migrationBuilder.AddColumn<int>(
-                name: "UsersCount",
-                table: "PeriodicCredentialReports",
-                type: "INTEGER",
-                nullable: false,
-                defaultValue: 0);
+        migrationBuilder.AddColumn<int>(
+            name: "UsersCount",
+            table: "PeriodicCredentialReports",
+            type: "INTEGER",
+            nullable: false,
+            defaultValue: 0);
 
-            migrationBuilder.AddPrimaryKey(
-                name: "PK_PeriodicCredentialReports",
-                table: "PeriodicCredentialReports",
-                columns: new[] { "Tenant", "CreatedAt" });
+        migrationBuilder.AddPrimaryKey(
+            name: "PK_PeriodicCredentialReports",
+            table: "PeriodicCredentialReports",
+            columns: new[] { "Tenant", "CreatedAt" });
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_PeriodicCredentialReports_AccountInfo_Tenant",
-                table: "PeriodicCredentialReports",
-                column: "Tenant",
-                principalTable: "AccountInfo",
-                principalColumn: "AcountName",
-                onDelete: ReferentialAction.Cascade);
-        }
+        migrationBuilder.AddForeignKey(
+            name: "FK_PeriodicCredentialReports_AccountInfo_Tenant",
+            table: "PeriodicCredentialReports",
+            column: "Tenant",
+            principalTable: "AccountInfo",
+            principalColumn: "AcountName",
+            onDelete: ReferentialAction.Cascade);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropForeignKey(
-                name: "FK_PeriodicCredentialReports_AccountInfo_Tenant",
-                table: "PeriodicCredentialReports");
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropForeignKey(
+            name: "FK_PeriodicCredentialReports_AccountInfo_Tenant",
+            table: "PeriodicCredentialReports");
 
-            migrationBuilder.DropPrimaryKey(
-                name: "PK_PeriodicCredentialReports",
-                table: "PeriodicCredentialReports");
+        migrationBuilder.DropPrimaryKey(
+            name: "PK_PeriodicCredentialReports",
+            table: "PeriodicCredentialReports");
 
-            migrationBuilder.DropColumn(
-                name: "CreatedAt",
-                table: "PeriodicCredentialReports");
+        migrationBuilder.DropColumn(
+            name: "CreatedAt",
+            table: "PeriodicCredentialReports");
 
-            migrationBuilder.DropColumn(
-                name: "CredentialsCount",
-                table: "PeriodicCredentialReports");
+        migrationBuilder.DropColumn(
+            name: "CredentialsCount",
+            table: "PeriodicCredentialReports");
 
-            migrationBuilder.DropColumn(
-                name: "UsersCount",
-                table: "PeriodicCredentialReports");
+        migrationBuilder.DropColumn(
+            name: "UsersCount",
+            table: "PeriodicCredentialReports");
 
-            migrationBuilder.RenameTable(
-                name: "PeriodicCredentialReports",
-                newName: "PeriodicCredentialReport");
+        migrationBuilder.RenameTable(
+            name: "PeriodicCredentialReports",
+            newName: "PeriodicCredentialReport");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "Tenant",
-                table: "PeriodicCredentialReport",
-                type: "TEXT",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "TEXT");
+        migrationBuilder.AlterColumn<string>(
+            name: "Tenant",
+            table: "PeriodicCredentialReport",
+            type: "TEXT",
+            nullable: true,
+            oldClrType: typeof(string),
+            oldType: "TEXT");
 
-            migrationBuilder.AddForeignKey(
-                name: "FK_PeriodicCredentialReport_AccountInfo_Tenant",
-                table: "PeriodicCredentialReport",
-                column: "Tenant",
-                principalTable: "AccountInfo",
-                principalColumn: "AcountName",
-                onDelete: ReferentialAction.Cascade);
-        }
+        migrationBuilder.AddForeignKey(
+            name: "FK_PeriodicCredentialReport_AccountInfo_Tenant",
+            table: "PeriodicCredentialReport",
+            column: "Tenant",
+            principalTable: "AccountInfo",
+            principalColumn: "AcountName",
+            onDelete: ReferentialAction.Cascade);
     }
 }
