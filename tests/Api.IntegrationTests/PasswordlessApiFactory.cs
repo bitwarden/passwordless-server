4c14ab2c3c82aa591c0b65e2915d89c312dd04b3
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2024-01-11 22:03:25 +0200

    Allow other logging handlers to work

--- tests/Api.IntegrationTests/PasswordlessApiFactory.cs
+++ tests/Api.IntegrationTests/PasswordlessApiFactory.cs
@@ -24,26 +24,24 @@ public partial class PasswordlessApiFactory : WebApplicationFactory<Program>, IA
 
     public FakeTimeProvider TimeProvider { get; } = new();
 
-    protected override void ConfigureWebHost(IWebHostBuilder builder)
-    {
+    protected override void ConfigureWebHost(IWebHostBuilder builder) =>
         builder
             .UseSetting("ConnectionStrings:sqlite:api", string.Empty)
             .UseSetting("ConnectionStrings:mssql:api", _dbContainer.GetConnectionString())
-            .ConfigureLogging(loggingBuilder => loggingBuilder.ClearProviders().AddXUnit(this))
+            .ConfigureLogging(logging => logging.ClearProviders().AddXUnit(this))
             .ConfigureTestServices(services =>
             {
                 services.RemoveAll(typeof(IHostedService));
                 services.RemoveAll(typeof(TimeProvider));
                 services.AddSingleton<TimeProvider>(TimeProvider);
             });
-    }
 
     public async Task InitializeAsync()
     {
         await _dbContainer.StartAsync();
 
         // Perform migrations and make sure the API is ready to receive requests
-        using var httpClient = this.CreateClient();
+        using var httpClient = CreateClient();
         using var response = await httpClient.GetAsync("/");
         response.EnsureSuccessStatusCode();
     }
