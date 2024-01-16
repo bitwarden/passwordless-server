5569470edb9844ee2c6d719d1ed595f75d745c60
jrmccannon <jared.r.mccannon@gmail.com>
2024-01-16 11:47:09 -0600

    Updated the email template.

--- src/Service/MagicLinks/MagicLinkService.cs
+++ src/Service/MagicLinks/MagicLinkService.cs
@@ -25,9 +25,38 @@ public class MagicLinkService
         {
             To = new[] { dto.EmailAddress.ToString() },
             From = "do-not-reply@passwordless.dev",
-            Subject = "Magic Link",
-            TextBody = $"Click the link to sign in: {link}",
-            HtmlBody = $"<a href=\"{link}\">Click here</a> to sign in"
+            Subject = "Verify Email Address",
+            TextBody = $"Please click the link to sign in: {link}",
+            HtmlBody =
+                //lang=html
+                $$"""
+                <!DOCTYPE html>
+                <html lang="en">
+                	<head>
+                		<title>Sign-In With Link Request</title>
+                	</head>
+                	<body style="
+                	        background-color: white;
+                            font-family: DM Sans,system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Helvetica Neue,sans-serif;
+                            font-size: 1rem;">
+                		<h3 style="text-align: center;">Hello</h3>
+                		<p style="text-align: center;">Please click the button below to log in.</p>
+                		<a href="{link}" style="
+                		    width: 2.5rem;
+                		    margin-left: auto;
+                		    margin-right: auto;
+                		    padding: .75rem 1.5rem;
+                    		background-color: rgb(18 82 163); 
+                    		border-radius: 999px;
+                    		color: white;
+                    		text-align: center; 
+                    		text-decoration: none; 
+                    		display: block;
+                    		cursor: pointer;">Login</a>
+                		<p style="text-align: center;">If you did not request this email, it is safe to ignore.</p>
+                	</body>
+                </html>
+                """
         });
 
         return new MagicLinkResult();
@@ -41,6 +70,5 @@ public class MagicLinkService
 
     public class MagicLinkResult : Result
     {
-
     }
 }
