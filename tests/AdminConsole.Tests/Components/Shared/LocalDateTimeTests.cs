3adabcf1dbfdaf674fe4b8f99fe11adfac6ffda4
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-01-23 11:24:28 +0100

    tests

--- tests/AdminConsole.Tests/Components/Shared/LocalDateTimeTests.cs
+++ tests/AdminConsole.Tests/Components/Shared/LocalDateTimeTests.cs
@@ -30,4 +30,25 @@ public class LocalDateTimeTests : TestContext
         Assert.Contains($"<time id=\"{cut.Instance.Id}\" datetime=\"1990-01-05T18:00:00\"></time>", actual);
         Assert.Contains($"document.getElementById(\"{cut.Instance.Id}\").innerText = new Date(\"1990-01-05T18:00:00\").toLocaleString();", actual);
     }
+
+    [Fact]
+    public void LocalDateTime_Applies_Nonce_OnScriptTag()
+    {
+        // Arrange
+        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+        httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext
+        {
+            Items = new Dictionary<object, object?> { { "csp-nonce", "hash" } }
+        });
+
+        Services.AddSingleton(httpContextAccessorMock.Object);
+        var cut = RenderComponent<LocalDateTime>(parameters => parameters
+            .Add(p => p.Value, new DateTime(1990, 01, 05, 18, 0, 0)));
+
+        // Act
+        var actual = cut.Markup;
+
+        // Assert
+        Assert.Contains($"<script nonce=\"hash\">", actual);
+    }
 }
