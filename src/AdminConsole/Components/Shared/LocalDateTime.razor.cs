5816d6d6f6634ad87bae10d076853ae578b33c24
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-01-23 16:27:32 +0100

    fix test

--- src/AdminConsole/Components/Shared/LocalDateTime.razor.cs
+++ src/AdminConsole/Components/Shared/LocalDateTime.razor.cs
@@ -7,17 +7,10 @@ namespace Passwordless.AdminConsole.Components.Shared;
 /// </summary>
 public partial class LocalDateTime : ComponentBase
 {
-    private readonly IHttpContextAccessor _httpContextAccessor;
-
-    public LocalDateTime(IHttpContextAccessor httpContextAccessor)
-    {
-        _httpContextAccessor = httpContextAccessor;
-    }
-
     public string Id { get; } = Guid.NewGuid().ToString();
 
     [Parameter]
     public required DateTime Value { get; set; }
 
-    public string CspNonce => _httpContextAccessor.HttpContext!.Items["csp-nonce"]!.ToString()!;
+    public string CspNonce => HttpContextAccessor.HttpContext!.Items["csp-nonce"]!.ToString()!;
 }
