4135b8bad4c1580eea304cb47c97f65d5ba9918d
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-01-24 13:27:03 +0100

    documentation

--- src/AdminConsole/Components/Pages/App/BaseApplicationPage.cs
+++ src/AdminConsole/Components/Pages/App/BaseApplicationPage.cs
@@ -4,6 +4,9 @@ namespace Passwordless.AdminConsole.Components.Pages.App;
 
 public abstract class BaseApplicationPage : ComponentBase
 {
+    /// <summary>
+    /// The route parameter for the application id for application scoped pages.
+    /// </summary>
     [Parameter]
     public required string App { get; set; }
 }
