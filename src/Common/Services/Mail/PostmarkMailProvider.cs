4809cb8c54ffbb7023481be332d3aa64110a2a76
jrmccannon <jared.r.mccannon@gmail.com>
2024-01-31 12:53:24 -0600

    Added logger and warning message in the event something is misconfigured with the message type. Added unit test to validate this.

--- src/Common/Services/Mail/PostmarkMailProvider.cs
+++ src/Common/Services/Mail/PostmarkMailProvider.cs
@@ -2,7 +2,7 @@ using PostmarkDotNet;
 
 namespace Passwordless.Common.Services.Mail;
 
-public class PostmarkMailProvider(PostmarkMailProviderConfiguration configuration) : IMailProvider
+public class PostmarkMailProvider(PostmarkMailProviderConfiguration configuration, ILogger<PostmarkMailProvider> logger) : IMailProvider
 {
     private readonly Dictionary<string, ConfiguredPostmarkClient> _clients = configuration.MessageStreams?
         .ToDictionary(x => x.Name, x => x.GetConfiguredClient()) ?? [];
@@ -12,6 +12,9 @@ public class PostmarkMailProvider(PostmarkMailProviderConfiguration configuratio
     {
         var configuredClient = _clients.GetValueOrDefault(message.MessageType) ?? _defaultClient;
 
+        if (!string.IsNullOrWhiteSpace(message.MessageType) && configuredClient == _defaultClient)
+            logger.LogWarning("MessageType {messageType} was set but default Postmark Mail client was used.", message.MessageType);
+        
         PostmarkMessage pm = new PostmarkMessage
         {
             To = string.Join(',', message.To),
