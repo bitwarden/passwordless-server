d44e4a6e6fa65666f39ba15b0e35cc1b2998e93d
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-02-07 12:33:26 +0100

    Fix

--- src/Service/MDS/CacheHandler.cs
+++ src/Service/MDS/CacheHandler.cs
@@ -1,30 +1,66 @@
+using System.Net;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace Passwordless.Service.MDS;
 
 /// <summary>
-/// The cache handler will attempt to download the latest MDS blob and cache it locally. The cache will be used
-/// when the download fails. Use this if the intent is to automatically synchronize with the latest MDS blob from
-/// the FIDO Alliance.
+/// The OfflineCacheHandler will attempt to read the MDS blob from the local file system. Use this if the intent is
+/// to manually synchronize with the latest MDS blob from the FIDO Alliance. This is useful for self-hosted
+/// deployments where there is no connectivity to the FIDO Alliance or internet.
 /// </summary>
-public class CacheHandler : DelegatingHandler
+public sealed class CacheHandler : DelegatingHandler
 {
+    private const string Path = "mds.jwt";
+
+    private readonly IConfiguration _configuration;
     private readonly ILogger<CacheHandler> _logger;
 
-    public CacheHandler(ILogger<CacheHandler> logger)
+    public CacheHandler(
+        IConfiguration configuration,
+        ILogger<CacheHandler> logger)
     {
-        _logger = logger;
-    }
-
-    public CacheHandler(HttpMessageHandler httpMessageHandler, ILogger<CacheHandler> logger) : base(httpMessageHandler)
-    {
+        _configuration = configuration;
         _logger = logger;
     }
 
     protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("[CacheHandler] Attempting to synchronize with the latest blob from FIDO Alliance.");
+        HttpResponseMessage? response = null;
 
-        return await base.SendAsync(request, cancellationToken);
+        // If we're running in online mode, move to the next handler, or execute the request if this is the last handler.
+        if (string.Equals(_configuration["fido2:mds:mode"], "Online", StringComparison.InvariantCultureIgnoreCase))
+        {
+            try
+            {
+                response = await base.SendAsync(request, cancellationToken);
+            }
+            catch (HttpRequestException)
+            {
+                // Request failed, can happen when domain name cannot be resolved, etc.
+                _logger.LogInformation("[OfflineCacheHandler] Synchronizing using the MDS blob with FIDO2 failed.");
+            }
+        }
+
+        // If the response has failed, we need to attempt to read the MDS blob from the local file system.
+        if (response == null || !response.IsSuccessStatusCode)
+        {
+            _logger.LogInformation("[OfflineCacheHandler] Synchronizing using the MDS blob found at '{Path}'.", Path);
+            if (!File.Exists(Path))
+            {
+                _logger.LogError("[OfflineCacheHandler] No offline MDS blob found at '{Path}'.", Path);
+            }
+
+            var jwt = await File.ReadAllTextAsync(Path, cancellationToken);
+            var content = new StringContent(jwt);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+
+            response = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = content,
+            };
+        }
+
+        return response;
     }
 }
--- src/Service/MDS/MetaDataServiceBootstrap.cs
+++ src/Service/MDS/MetaDataServiceBootstrap.cs
@@ -8,21 +8,13 @@ public static class MetaDataServiceBootstrap
 {
     public static void AddMetaDataService(this WebApplicationBuilder builder)
     {
-        var configuration = builder.Configuration;
-
         var httpClientBuilder = builder.Services.AddHttpClient(nameof(Fido2MetadataServiceRepository), client =>
         {
             client.BaseAddress = new Uri("https://mds3.fidoalliance.org/");
         });
 
-        builder.Services.AddTransient<OfflineCacheHandler>();
-        httpClientBuilder.AddHttpMessageHandler<OfflineCacheHandler>();
-
-        if (string.Equals(configuration["fido2:mds:mode"], "Online", StringComparison.InvariantCultureIgnoreCase))
-        {
-            builder.Services.AddTransient<CacheHandler>();
-            httpClientBuilder.AddHttpMessageHandler<CacheHandler>();
-        }
+        builder.Services.AddTransient<CacheHandler>();
+        httpClientBuilder.AddHttpMessageHandler<CacheHandler>();
 
         builder.Services.AddSingleton<IMetadataRepository, Fido2MetadataServiceRepository>();
         builder.Services.AddSingleton<IMetadataService, DistributedCacheMetadataService>();
