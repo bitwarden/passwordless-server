e0a117934fc66523fd37e7d2165f8c6fe290ef76
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-02-09 14:10:39 +0100

    SecureScript & SecureStylesheet with tests

--- tests/AdminConsole.Tests/Components/Shared/SecureScriptTests.cs
+++ tests/AdminConsole.Tests/Components/Shared/SecureScriptTests.cs
@@ -1,5 +1,7 @@
 using Bunit;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Passwordless.AdminConsole.Components.Shared;
 using Xunit;
@@ -8,12 +10,20 @@ namespace Passwordless.AdminConsole.Tests.Components.Shared;
 
 public class SecureScriptTests : TestContext
 {
+    private readonly Mock<IHttpContextAccessor> _httpContextAccessorMock = new();
+    private readonly Mock<IFileVersionProvider> _fileVersionProviderMock = new();
+
+    public SecureScriptTests()
+    {
+        this.Services.AddSingleton(_httpContextAccessorMock.Object);
+        this.Services.AddSingleton(_fileVersionProviderMock.Object);
+    }
+
     [Fact]
     public void SecureScript_Renders_NonceAttribute()
     {
         // Arrange
-        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
-        httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext
+        _httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext
         {
             Items = new Dictionary<object, object?>()
             {
@@ -25,6 +35,50 @@ public class SecureScriptTests : TestContext
         var cut = RenderComponent<SecureScript>();
 
         // Assert
-        cut.MarkupMatches($"<script nonce=\"test-nonce\" diff:ignoreChildren></script>");
+        cut.MarkupMatches("<script nonce=\"test-nonce\" diff:ignoreChildren></script>");
+    }
+
+    [Fact]
+    public void SecureScript_Renders_WithoutNonceAttribute()
+    {
+        // Arrange
+
+        // Act
+        var cut = RenderComponent<SecureScript>();
+
+        // Assert
+        cut.MarkupMatches("<script diff:ignoreChildren></script>");
+    }
+
+    [Fact]
+    public void SecureScript_Renders_InlineScript()
+    {
+        // Arrange
+
+        // Act
+        var cut = RenderComponent<SecureScript>(parameters => parameters
+            .Add(p => p.ChildContent, "console.log('Hello, World!');"));
+
+        // Assert
+        cut.MarkupMatches("<script diff:ignoreAttributes>console.log('Hello, World!');</script>");
+    }
+
+    [Fact]
+    public void SecureScript_Renders_ScriptFile()
+    {
+        // Arrange
+        _fileVersionProviderMock.Setup(x => x.AddFileVersionToPath("", "test.js")).Returns("test.js?v=1");
+        var additionalAttributes = new Dictionary<string, object>
+        {
+            { "src", "test.js" },
+            { "type", "module" }
+        };
+
+        // Act
+        var cut = RenderComponent<SecureScript>(parameters => parameters
+            .Add(p => p.AdditionalAttributes, additionalAttributes));
+
+        // Assert
+        cut.MarkupMatches("<script src=\"test.js?v=1\" type=\"module\" diff:ignoreChildren></script>");
     }
 }
