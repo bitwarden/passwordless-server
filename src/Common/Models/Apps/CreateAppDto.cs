557c12041a2eef993ac13c47d4f1e5224a142959
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2024-02-12 17:57:41 +0200

    asd

--- src/Common/Models/Apps/CreateAppDto.cs
+++ src/Common/Models/Apps/CreateAppDto.cs
@@ -3,8 +3,10 @@
 public record CreateAppDto
 {
     public string AdminEmail { get; set; } = "";
+
     public bool EventLoggingIsEnabled { get; set; } = false;
     public int EventLoggingRetentionPeriod { get; set; } = 365;
+    public int MagicLinkEmailMonthlyQuota { get; set; }
 
     /// <summary>
     /// Maximum number of users allowed for this application.
