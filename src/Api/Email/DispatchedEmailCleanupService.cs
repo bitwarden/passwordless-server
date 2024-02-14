6edbbcdc179b6eee95f76ca6f985298d5be0d338
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-02-14 17:23:10 +0100

    Fix background service

--- src/Api/Email/DispatchedEmailCleanupService.cs
+++ src/Api/Email/DispatchedEmailCleanupService.cs
@@ -6,13 +6,19 @@ namespace Passwordless.Api.Email;
 public class DispatchedEmailCleanupService(
     TimeProvider timeProvider,
     ILogger<DispatchedEmailCleanupService> logger,
-    ITenantStorage tenantStorage)
+    IServiceProvider serviceProvider)
     : BasePeriodicBackgroundService(
         new TimeOnly(00, 00, 00),
         TimeSpan.FromDays(1),
         timeProvider, logger)
 {
-    protected override async Task DoWorkAsync(CancellationToken cancellationToken) =>
+    protected override async Task DoWorkAsync(CancellationToken cancellationToken)
+    {
+        var scope = serviceProvider.CreateScope();
+        var tenantStorage = scope.ServiceProvider.GetRequiredService<ITenantStorage>();
+
         // We only need 30 days worth of emails, but let's keep a small extra buffer just in case
         await tenantStorage.DeleteOldDispatchedEmailsAsync(TimeSpan.FromDays(50));
+
+    }
 }
