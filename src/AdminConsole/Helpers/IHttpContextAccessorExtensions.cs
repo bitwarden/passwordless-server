bb4a67ae79b6d5ea1ba00bd0277fb657c992b603
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-02-14 16:11:28 +0100

    doc

--- src/AdminConsole/Helpers/IHttpContextAccessorExtensions.cs
+++ src/AdminConsole/Helpers/IHttpContextAccessorExtensions.cs
@@ -4,6 +4,11 @@ namespace Passwordless.AdminConsole.Helpers;
 
 public static class IHttpContextAccessorExtensions
 {
+    /// <summary>
+    /// Detect whether the rendered page is a Razor Page or something else (Blazor).
+    /// </summary>
+    /// <param name="httpContextAccessor"></param>
+    /// <returns></returns>
     public static bool IsRazorPages(this IHttpContextAccessor httpContextAccessor)
     {
         var metadata = httpContextAccessor.HttpContext?.GetEndpoint()?.Metadata.GetMetadata<PageModelAttribute>();
