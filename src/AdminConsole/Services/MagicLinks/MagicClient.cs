48caf3a5ca034b680a99d21ef2716114955694fe
Anders Åberg <anders@andersaberg.com>
2024-02-15 16:24:10 +0100

    New file

---
+++ src/AdminConsole/Services/MagicLinks/MagicClient.cs
@@ -0,0 +1,36 @@
+using Microsoft.Extensions.Options;
+
+namespace Passwordless.AdminConsole.Services.MagicLinks;
+
+public class MagicClient : PasswordlessClient {
+    
+    private readonly HttpClient _http;
+    private readonly PasswordlessOptions _options;
+
+    public MagicClient(HttpClient http, IOptions<PasswordlessOptions> options) : base(http, new PasswordlessOptions { ApiUrl = options.Value.ApiUrl, ApiSecret = options.Value.ApiSecret})
+    {
+        _http = new HttpClient(new PasswordlessHttpHandler(http, true), true)
+        {
+            BaseAddress = new Uri(options.Value.ApiUrl),
+            DefaultRequestHeaders =
+            {
+                {"ApiSecret", options.Value.ApiSecret}
+            }
+        };
+
+        _options = options.Value;
+    }
+
+    public async Task SendMagicLinkAsync(string userId, string emailAddress, string urlTemplate)
+    {
+        using var response = await _http.PostAsJsonAsync("/magic-link/send", new
+        {
+            userId, emailAddress, urlTemplate
+        });
+        
+        var xx = response.Content.ReadAsStringAsync();
+        
+        response.EnsureSuccessStatusCode();
+    }
+    
+}
