654b25b5bcd4fef86b2048b28928bdfb564f39e2
jrmccannon <jared.r.mccannon@gmail.com>
2024-02-21 10:28:25 -0600

    formatting.

--- src/Service/Models/SigninTokenRequest.cs
+++ src/Service/Models/SigninTokenRequest.cs
@@ -6,9 +6,9 @@ namespace Passwordless.Service.Models;
 public class SigninTokenRequest : RequestBase
 {
     private static readonly TimeSpan DefaultTimeToLive = TimeSpan.FromSeconds(120);
-    
+
     public required string UserId { get; init; }
-    
+
     /// <summary>
     /// Time to live is the number of seconds the token has before it expires.
     /// </summary>
