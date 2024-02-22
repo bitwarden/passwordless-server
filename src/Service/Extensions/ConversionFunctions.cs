4e05e91be19fcf9b39807ae045f200eee8a53920
Jared McCannon <jmccannon@bitwarden.com>
2024-02-22 11:01:11 -0600

    Update src/Service/Extensions/ConversionFunctions.cs
    Co-authored-by: Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>

--- src/Service/Extensions/ConversionFunctions.cs
+++ src/Service/Extensions/ConversionFunctions.cs
@@ -2,5 +2,5 @@ namespace Passwordless.Service.Extensions;
 
 public static class ConversionFunctions
 {
-    public static TimeSpan? ToTimeSpanFromSeconds(this int seconds) => TimeSpan.FromSeconds(seconds);
+    public static TimeSpan ToTimeSpanFromSeconds(this int seconds) => TimeSpan.FromSeconds(seconds);
 }
