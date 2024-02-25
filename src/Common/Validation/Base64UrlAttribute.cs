c9816d53ecff12494ca094eec95863aa4aaff3ae
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-02-25 15:54:27 +0100

    shorter

--- src/Common/Validation/Base64UrlAttribute.cs
+++ src/Common/Validation/Base64UrlAttribute.cs
@@ -4,9 +4,4 @@ using Passwordless.Common.Constants;
 namespace Passwordless.Common.Validation;
 
 [AttributeUsage(AttributeTargets.Property)]
-public sealed class Base64UrlAttribute : RegularExpressionAttribute
-{
-    public Base64UrlAttribute() : base(RegularExpressions.Base64Url)
-    {
-    }
-}
+public sealed class Base64UrlAttribute() : RegularExpressionAttribute(RegularExpressions.Base64Url);
