7700294b14dd7f079281c45d72df3d34d99a9480
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-02-27 11:16:20 +0100

    Don't allow any tags

--- src/Common/Serialization/HtmlSanitizer.cs
+++ src/Common/Serialization/HtmlSanitizer.cs
@@ -1,8 +1,13 @@
+using Ganss.Xss;
+
 namespace Passwordless.Common.Serialization;
 
 public static class HtmlSanitizer
 {
-    private static readonly Ganss.Xss.HtmlSanitizer _htmlSanitizer = new();
+    private static readonly Ganss.Xss.HtmlSanitizer _htmlSanitizer = new(new HtmlSanitizerOptions
+    {
+        AllowedTags = new HashSet<string>(0)
+    });
 
     public static string Sanitize(string input)
     {
