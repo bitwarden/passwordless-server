0181155732f1d31de0fcd999f16f46e780cbe840
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-02-27 13:14:34 +0100

    Fix tests

--- src/Common/Models/Apps/CreateApplicationRequest.cs
+++ src/Common/Models/Apps/CreateApplicationRequest.cs
@@ -5,7 +5,8 @@ namespace Passwordless.Common.Models.Apps;
 
 public class CreateApplicationRequest
 {
-    [FromRoute, MinLength(3), RegularExpression("^[a-z]{1}[a-z0-9]{2,61}$")]
+    [FromRoute, MinLength(3)]
+    [RegularExpression("^[a-z]{1}[a-z0-9]{2,61}$", ErrorMessage = "'AppId' must be between 3 and 62 characters, contain only lowercase letters and numbers, and start with a lowercase letter.")]
     public required string AppId { get; set; }
 
     [FromBody]
