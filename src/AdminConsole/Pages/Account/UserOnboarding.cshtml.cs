c4ea229422c7c62f52a6bccf89142ef3d9bcd52e
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-02-29 11:18:40 +0100

    [PAS289][Defect] Feedback no longer a confirmation step when deleting user credentials (#439)

--- src/AdminConsole/Pages/Account/UserOnboarding.cshtml.cs
+++ src/AdminConsole/Pages/Account/UserOnboarding.cshtml.cs
@@ -1,6 +1,5 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Passwordless.AdminConsole.Pages.Shared;
 using Passwordless.AdminConsole.Services.AuthenticatorData;
 
 namespace Passwordless.AdminConsole.Pages.Account;
@@ -14,18 +13,13 @@ public class UserOnboarding : PageModel
         IAuthenticatorDataProvider authenticatorDataProvider)
     {
         _passwordlessClient = passwordlessClient;
-        Credentials = new CredentialsModel(authenticatorDataProvider)
-        {
-            HideDetails = true
-        };
     }
 
-    public CredentialsModel Credentials { get; set; }
+    public IReadOnlyCollection<Credential> Credentials { get; set; }
 
     public async Task OnGet()
     {
-        var items = await _passwordlessClient.ListCredentialsAsync(HttpContext.User.Claims
+        Credentials = await _passwordlessClient.ListCredentialsAsync(HttpContext.User.Claims
             .FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value);
-        Credentials.SetItems(items);
     }
 }
