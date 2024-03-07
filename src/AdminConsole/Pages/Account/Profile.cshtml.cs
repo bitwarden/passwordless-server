03a78b56e680893d5ffb640ef07c45299996d0d8
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-03-07 14:41:06 +0100

    Fix delete passkey bug once and for all

--- src/AdminConsole/Pages/Account/Profile.cshtml.cs
+++ src/AdminConsole/Pages/Account/Profile.cshtml.cs
@@ -1,39 +1,9 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Passwordless.AdminConsole.Identity;
-using Passwordless.AdminConsole.Services.AuthenticatorData;
 
 namespace Passwordless.AdminConsole.Pages.Account;
 
 public class Profile : PageModel
 {
-    private readonly IPasswordlessClient _client;
-    private readonly UserManager<ConsoleAdmin> _userManager;
-
-    public Profile(
-        IPasswordlessClient client,
-        UserManager<ConsoleAdmin> userManager,
-        IAuthenticatorDataProvider authenticatorDataProvider)
-    {
-        _client = client;
-        _userManager = userManager;
-    }
-
-    public IReadOnlyCollection<Credential> Credentials { get; set; }
-
-    public async Task<IActionResult> OnPostRemoveCredential(string credentialId)
-    {
-        await _client.DeleteCredentialAsync(credentialId);
-        return RedirectToPage();
-    }
-
-    public async Task OnGet()
-    {
-        var userId = _userManager.GetUserId(HttpContext.User);
-        if (userId != null)
-        {
-            Credentials = await _client.ListCredentialsAsync(userId);
-        }
-    }
+    public string UserId => HttpContext.User.Claims.Single(c => c.Type == ClaimTypes.NameIdentifier).Value;
 }
--- src/AdminConsole/Pages/App/Credentials/User.cshtml.cs
+++ src/AdminConsole/Pages/App/Credentials/User.cshtml.cs
@@ -7,10 +7,6 @@ namespace Passwordless.AdminConsole.Pages.App.Credentials;
 
 public class UserModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
-    private readonly IScopedPasswordlessClient _passwordlessClient;
-
-    public IReadOnlyCollection<Credential> Credentials { get; set; }
     public IReadOnlyCollection<AliasPointer> Aliases { get; set; }
 
     [BindProperty(SupportsGet = true)]
@@ -18,22 +14,15 @@ public class UserModel : PageModel
 
     public UserModel(
         IAuthenticatorDataProvider authenticatorDataProvider,
-        ILogger<IndexModel> logger,
         IScopedPasswordlessClient api)
     {
-        _logger = logger;
-        _passwordlessClient = api;
+        PasswordlessClient = api;
     }
+
+    public IScopedPasswordlessClient PasswordlessClient { get; }
 
     public async Task OnGet()
     {
-        Credentials = await _passwordlessClient.ListCredentialsAsync(UserId);
-        Aliases = await _passwordlessClient.ListAliasesAsync(UserId);
-    }
-
-    public async Task<IActionResult> OnPostRemoveCredential(string credentialId)
-    {
-        await _passwordlessClient.DeleteCredentialAsync(credentialId);
-        return RedirectToPage();
+        Aliases = await PasswordlessClient.ListAliasesAsync(UserId);
     }
 }
