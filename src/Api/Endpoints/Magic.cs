f18e81d65bb22eb64757ee33710dc31ee337a2d6
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2024-03-12 15:34:41 +0200

    Temporarily disable rate limiting in magic links endpoint (#463)

--- src/Api/Endpoints/Magic.cs
+++ src/Api/Endpoints/Magic.cs
@@ -47,7 +47,6 @@ public static class MagicEndpoints
             })
             .WithParameterValidation()
             .RequireSecretKey()
-            .RequireCors("default")
-            .RequireRateLimiting(RateLimiterPolicy);
+            .RequireCors("default");
     }
 }
