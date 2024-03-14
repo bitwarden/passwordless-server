a8b84226e005784aa864d5bdfa18feb6270839c3
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-03-14 16:20:40 +0100

    Query documentation

--- src/Api/Endpoints/Authenticators.cs
+++ src/Api/Endpoints/Authenticators.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Passwordless.Api.Authorization;
 using Passwordless.Api.OpenApi;
+using Passwordless.Api.OpenApi.Extensions;
 using Passwordless.Common.Models.Authenticators;
 using Passwordless.Service;
 using Passwordless.Service.EventLog.Loggers;
@@ -22,7 +23,7 @@ public static class AuthenticatorsEndpoints
         group.MapGet("/list", ListConfiguredAuthenticatorsAsync)
             .WithOpenApi(o =>
             {
-                o.Parameters.First(p => p.Name == nameof(ConfiguredAuthenticatorRequest.IsAllowed)).Description = "When 'true', all authenticators on the allowlist are returned. When 'false', all authenticators on the blocklist are returned.";
+                o.Parameters.Get(nameof(ConfiguredAuthenticatorRequest.IsAllowed)).Description = "When 'true', all authenticators on the allowlist are returned. When 'false', all authenticators on the blocklist are returned.";
                 return o;
             });
 
