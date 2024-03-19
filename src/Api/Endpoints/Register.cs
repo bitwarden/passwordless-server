5e791fd9dd56539a8e11aff742ce6a86dc09192a
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-03-19 10:04:41 +0100

    PAS-202 | OpenAPI: Registration

--- src/Api/Endpoints/Register.cs
+++ src/Api/Endpoints/Register.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Passwordless.Api.Authorization;
 using Passwordless.Api.OpenApi;
 using Passwordless.Common.Constants;
@@ -17,45 +18,54 @@ public static class RegisterEndpoints
             .RequireCors("default")
             .WithTags(OpenApiTags.Registration);
 
-        group.MapPost("/token", async (
-                RegisterToken registerToken,
-                IFido2Service fido2Service,
-                CancellationToken token
-            ) =>
-            {
-                var result = await fido2Service.CreateRegisterTokenAsync(registerToken);
-                return Ok(new RegisterTokenResponse(result));
-            })
+        group.MapPost("/token", RegisterAsync)
             .RequireSecretKey(SecretKeyScopes.TokenRegister)
             .WithParameterValidation();
 
-        group.MapPost("/begin", async (
-                FidoRegistrationBeginDTO payload,
-                IFido2Service fido2Service,
-                CancellationToken token
-            ) =>
-            {
-                var result = await fido2Service.RegisterBeginAsync(payload);
-                return Ok(result);
-            })
+        group.MapPost("/begin", RegisterBeginAsync)
             .RequirePublicKey(PublicKeyScopes.Register)
             .WithMetadata(new HttpMethodMetadata(new[] { "POST" }, acceptCorsPreflight: true));
 
-        group.MapPost("/complete", async (
-                RegistrationCompleteDTO payload,
-                HttpRequest request,
-                IFido2Service fido2Service,
-                CancellationToken token
-            ) =>
-            {
-                var (deviceInfo, country) = Extensions.Helpers.GetDeviceInfo(request);
-                var result = await fido2Service.RegisterCompleteAsync(payload, deviceInfo, country);
-
-                // Avoid serializing the certificate
-                return Ok(result);
-            })
+        group.MapPost("/complete", RegisterCompleteAsync)
+            .RequirePublicKey(PublicKeyScopes.Register)
             .WithParameterValidation()
-            .RequirePublicKey(PublicKeyScopes.Register)
             .WithMetadata(new HttpMethodMetadata(new[] { "POST" }, acceptCorsPreflight: true));
+    }
+
+    /// <summary>
+    /// Registration (Step 1 - Server)
+    /// </summary>
+    public static async Task<IResult> RegisterAsync(
+        [FromBody] RegisterToken request,
+        [FromServices] IFido2Service fido2Service)
+    {
+        var result = await fido2Service.CreateRegisterTokenAsync(request);
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// Registration (Step 2 - Client): Get credential creation options.
+    /// </summary>
+    public static async Task<IResult> RegisterBeginAsync(
+        [FromBody] FidoRegistrationBeginDTO payload,
+        [FromServices] IFido2Service fido2Service)
+    {
+        var result = await fido2Service.RegisterBeginAsync(payload);
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// Registration (Step 3 - Client): Stores the created public key.
+    /// </summary>
+    public static async Task<IResult> RegisterCompleteAsync(
+        [FromBody] RegistrationCompleteDTO payload,
+        [FromServices] HttpRequest request,
+        [FromServices] IFido2Service fido2Service)
+    {
+        var (deviceInfo, country) = Extensions.Helpers.GetDeviceInfo(request);
+        var result = await fido2Service.RegisterCompleteAsync(payload, deviceInfo, country);
+
+        // Avoid serializing the certificate
+        return Ok(result);
     }
 }
