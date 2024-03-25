68e569651a99cce1d1f3783ed8fe7865943486f4
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-03-25 08:53:08 +0100

    Refactor

--- src/AdminConsole/Components/Shared/Alert.razor.cs
+++ src/AdminConsole/Components/Shared/Alert.razor.cs
@@ -4,6 +4,10 @@ namespace Passwordless.AdminConsole.Components.Shared;
 
 public partial class Alert : ComponentBase
 {
+    private const string BaseContainerClass = "rounded-md p-4";
+    private const string BaseIconClass = "h-5 w-5";
+    private const string BaseContentClass = "text-sm";
+
     [Parameter]
     public required RenderFragment ChildContent { get; set; }
 
@@ -21,38 +25,31 @@ public partial class Alert : ComponentBase
 
     protected override void OnInitialized()
     {
-        const string baseContainerClass = "rounded-md p-4";
-        const string baseIconClass = "h-5 w-5";
-        const string baseContentClass = "text-sm";
-
         switch (Style)
         {
             case ContextualStyles.Primary:
             case ContextualStyles.Info:
-                ContainerClass = string.Join(" ", baseContainerClass, "bg-blue-50");
-                IconClass = string.Join(" ", baseIconClass, "text-blue-400");
-                ContentClass = string.Join(" ", baseContentClass, "text-blue-700");
+                SetClasses("bg-blue-50", "text-blue-400", "text-blue-700");
                 break;
             case ContextualStyles.Warning:
-                ContainerClass = string.Join(" ", baseContainerClass, "bg-yellow-50");
-                IconClass = string.Join(" ", baseIconClass, "text-yellow-400");
-                ContentClass = string.Join(" ", baseContentClass, "text-yellow-700");
+                SetClasses("bg-yellow-50", "text-yellow-400", "text-yellow-700");
                 break;
             case ContextualStyles.Danger:
-                ContainerClass = string.Join(" ", baseContainerClass, "bg-red-50");
-                IconClass = string.Join(" ", baseIconClass, "text-red-400");
-                ContentClass = string.Join(" ", baseContentClass, "text-red-700");
+                SetClasses("bg-red-50", "text-red-400", "text-red-700");
                 break;
             case ContextualStyles.Success:
-                ContainerClass = string.Join(" ", baseContainerClass, "bg-green-50");
-                IconClass = string.Join(" ", baseIconClass, "text-green-400");
-                ContentClass = string.Join(" ", baseContentClass, "text-green-700");
+                SetClasses("bg-green-50", "text-green-400", "text-green-700");
                 break;
             case ContextualStyles.Secondary:
-                ContainerClass = string.Join(" ", baseContainerClass, "bg-gray-50");
-                IconClass = string.Join(" ", baseIconClass, "text-gray-400");
-                ContentClass = string.Join(" ", baseContentClass, "text-gray-700");
+                SetClasses("bg-gray-50", "text-gray-400", "text-gray-700");
                 break;
         }
+    }
+
+    private void SetClasses(string containerClass, string iconClass, string contentClass)
+    {
+        ContainerClass = string.Join(" ", BaseContainerClass, containerClass);
+        IconClass = string.Join(" ", BaseIconClass, iconClass);
+        ContentClass = string.Join(" ", BaseContentClass, contentClass);
     }
 }
