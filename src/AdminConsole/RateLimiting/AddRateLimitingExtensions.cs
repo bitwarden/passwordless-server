71ea0d6c90b73238b0c97183cc028a82ae5b5bde
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2024-03-27 20:58:59 +0200

    Fix status code

--- src/AdminConsole/RateLimiting/AddRateLimitingExtensions.cs
+++ src/AdminConsole/RateLimiting/AddRateLimitingExtensions.cs
@@ -3,5 +3,11 @@ namespace Passwordless.AdminConsole.RateLimiting;
 public static class AddRateLimitingExtensions
 {
     public static IServiceCollection AddRateLimiting(this IServiceCollection services) =>
-        services.AddRateLimiter(limiter => limiter.AddAdminPageRateLimitPolicy());
+        services.AddRateLimiter(limiter =>
+        {
+            // Reject with 429 instead of the default 503
+            limiter.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+
+            limiter.AddAdminPageRateLimitPolicy();
+        });
 }
