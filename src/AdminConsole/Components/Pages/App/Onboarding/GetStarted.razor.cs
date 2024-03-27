32035534b6a603694a19ed59e9512ceebd585374
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-03-27 15:57:14 +0100

    review

--- src/AdminConsole/Components/Pages/App/Onboarding/GetStarted.razor.cs
+++ src/AdminConsole/Components/Pages/App/Onboarding/GetStarted.razor.cs
@@ -6,22 +6,19 @@ namespace Passwordless.AdminConsole.Components.Pages.App.Onboarding;
 
 public partial class GetStarted : BaseApplicationPage
 {
-    public IEnumerable<LinkCard.LinkCardModel> BackendCards => new LinkCard.LinkCardModel[]
-    {
-        new("REST API", "Use our simple REST API", typeof(OpenApiIcon), new Uri(Configuration.IsSelfHosted() ? $"{PasswordlessOptions.Value.ApiUrl}/swagger/index.html" : "https://docs.passwordless.dev/guide/api-documentation.html")),
-        new(".NET", "Docs + Examples", typeof(DotNetIcon), new Uri("https://docs.passwordless.dev/guide/backend/dotnet.html")),
-        new("Java", "Docs + Examples", typeof(JavaIcon), new Uri("https://docs.passwordless.dev/guide/backend/java.html")),
-        new("Node.js", "Docs + Examples", typeof(NodeJsIcon), new Uri("https://docs.passwordless.dev/guide/backend/nodejs.html")),
-        new("Python", "Docs + Examples", typeof(PythonIcon), new Uri("https://docs.passwordless.dev/guide/backend/python3.html"))
-    };
+    private IEnumerable<LinkCard.LinkCardModel>? _backendCards;
+    private readonly IEnumerable<LinkCard.LinkCardModel> _frontendCards;
 
-    public IEnumerable<LinkCard.LinkCardModel> FrontendCards => new LinkCard.LinkCardModel[]
+    public GetStarted()
     {
-        new("Android", "Docs + Examples", typeof(AndroidIcon), new Uri("https://docs.passwordless.dev/guide/frontend/android.html")),
-        new("ASP.NET Identity", "Docs + Examples", typeof(DotNetIcon), new Uri("https://docs.passwordless.dev/guide/frontend/aspnet.html")),
-        new("Javascript", "Docs + Examples", typeof(JavascriptIcon), new Uri("https://docs.passwordless.dev/guide/frontend/javascript.html")),
-        new("React", "Docs + Examples", typeof(ReactIcon), new Uri("https://docs.passwordless.dev/guide/frontend/react.html"))
-    };
+        _frontendCards = new LinkCard.LinkCardModel[]
+        {
+            new("Android", "Docs + Examples", typeof(AndroidIcon), new Uri("https://docs.passwordless.dev/guide/frontend/android.html")),
+            new("ASP.NET Identity", "Docs + Examples", typeof(DotNetIcon), new Uri("https://docs.passwordless.dev/guide/frontend/aspnet.html")),
+            new("Javascript", "Docs + Examples", typeof(JavascriptIcon), new Uri("https://docs.passwordless.dev/guide/frontend/javascript.html")),
+            new("React", "Docs + Examples", typeof(ReactIcon), new Uri("https://docs.passwordless.dev/guide/frontend/react.html"))
+        };
+    }
 
     private bool _isInitialized;
 
@@ -31,6 +28,15 @@ public partial class GetStarted : BaseApplicationPage
 
     protected override async Task OnInitializedAsync()
     {
+        _backendCards = new LinkCard.LinkCardModel[]
+        {
+            new("REST API", "Use our simple REST API", typeof(OpenApiIcon), new Uri(Configuration.IsSelfHosted() ? $"{PasswordlessOptions.Value.ApiUrl}/swagger/index.html" : "https://docs.passwordless.dev/guide/api-documentation.html")),
+            new(".NET", "Docs + Examples", typeof(DotNetIcon), new Uri("https://docs.passwordless.dev/guide/backend/dotnet.html")),
+            new("Java", "Docs + Examples", typeof(JavaIcon), new Uri("https://docs.passwordless.dev/guide/backend/java.html")),
+            new("Node.js", "Docs + Examples", typeof(NodeJsIcon), new Uri("https://docs.passwordless.dev/guide/backend/nodejs.html")),
+            new("Python", "Docs + Examples", typeof(PythonIcon), new Uri("https://docs.passwordless.dev/guide/backend/python3.html"))
+        };
+
         var onboarding = await ApplicationService.GetOnboardingAsync(AppId);
         if (onboarding == null) throw new Exception("Onboarding not found");
 
