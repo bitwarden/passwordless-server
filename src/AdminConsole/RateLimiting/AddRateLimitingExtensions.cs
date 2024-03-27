8bae853fee9507b0d832c0d01ae2419e8964572a
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2024-03-27 21:20:28 +0200

    Add better formatting

--- src/AdminConsole/RateLimiting/AddRateLimitingExtensions.cs
+++ src/AdminConsole/RateLimiting/AddRateLimitingExtensions.cs
@@ -1,3 +1,6 @@
+using System.Globalization;
+using System.Threading.RateLimiting;
+
 namespace Passwordless.AdminConsole.RateLimiting;
 
 public static class AddRateLimitingExtensions
@@ -5,8 +8,22 @@ public static class AddRateLimitingExtensions
     public static IServiceCollection AddRateLimiting(this IServiceCollection services) =>
         services.AddRateLimiter(limiter =>
         {
-            // Reject with 429 instead of the default 503
-            limiter.RejectionStatusCode = StatusCodes.Status429TooManyRequests;
+            // Rate limiter terminates the request chain instead of throwing an exception,
+            // so the error will not reach the exception handler middleware.
+            // As a workaround, we provide some minimally empathetic feedback to the user here.
+            limiter.OnRejected = async (context, cancellationToken) =>
+            {
+                context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+
+                var retryAfterFormatted = context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter)
+                    ? retryAfter.TotalSeconds.ToString(CultureInfo.InvariantCulture)
+                    : "a few";
+
+                await context.HttpContext.Response.WriteAsync(
+                    $"Too many requests. Please try again after {retryAfterFormatted} second(s).",
+                    cancellationToken
+                );
+            };
 
             limiter.AddAdminPageRateLimitPolicy();
         });
