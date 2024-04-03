4231979575e6b89a21be9be7a618c62cb030be8d
jrmccannon <jared.r.mccannon@gmail.com>
2024-04-03 13:46:00 -0500

    Defaulting Aliases to be an empty list from the start.

--- src/Service/Models/UserSummary.cs
+++ src/Service/Models/UserSummary.cs
@@ -4,7 +4,7 @@ public class UserSummary
 {
     public string UserId { get; set; }
     public int AliasCount { get; set; }
-    public List<string> Aliases { get; set; }
+    public List<string> Aliases { get; set; } = [];
     public int CredentialsCount { get; set; }
     public DateTime? LastUsedAt { get; set; }
 }
