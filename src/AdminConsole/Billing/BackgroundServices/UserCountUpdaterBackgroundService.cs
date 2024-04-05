6e6ec8d179869e70b7e21a9c5b449bb1f9a958f1
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-04-05 11:18:34 +0200

    Improve startup time by scheduling jobs to run at fixed intervals.

--- src/AdminConsole/Billing/BackgroundServices/UserCountUpdaterBackgroundService.cs
+++ src/AdminConsole/Billing/BackgroundServices/UserCountUpdaterBackgroundService.cs
@@ -1,56 +1,32 @@
 using Passwordless.AdminConsole.Services;
+using Passwordless.Common.Background;
 
 namespace Passwordless.AdminConsole.Billing.BackgroundServices;
 
 /// <summary>
 /// Responsible for retrieving the current user count from the Passwordless API and storing it in the Admin Console.
 /// </summary>
-public sealed class UserCountUpdaterBackgroundService : BackgroundService
+public sealed class UserCountUpdaterBackgroundService(
+    IServiceProvider serviceProvider,
+    TimeProvider timeProvider,
+    ILogger<UserCountUpdaterBackgroundService> logger)
+    : BasePeriodicBackgroundService(
+        new TimeOnly(0),
+        TimeSpan.FromHours(1),
+        timeProvider,
+        logger)
 {
-    private readonly IServiceProvider _services;
-    private readonly ILogger<UserCountUpdaterBackgroundService> _logger;
-
-    public UserCountUpdaterBackgroundService(
-        IServiceProvider services,
-        ILogger<UserCountUpdaterBackgroundService> logger)
-    {
-        _services = services;
-        _logger = logger;
-    }
-
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        _logger.LogInformation($"{nameof(UserCountUpdaterBackgroundService)} is running.");
-        using PeriodicTimer timer = new(TimeSpan.FromHours(1));
-        try
-        {
-            await UpdateAsync();
-            while (await timer.WaitForNextTickAsync(stoppingToken))
-            {
-                await UpdateAsync();
-            }
-        }
-        catch (OperationCanceledException)
-        {
-            _logger.LogInformation($"{nameof(UserCountUpdaterBackgroundService)} is stopping.");
-        }
-        catch (Exception e)
-        {
-            _logger.LogCritical(e, $"{nameof(UserCountUpdaterBackgroundService)} failed.");
-        }
-    }
-
-    private async Task UpdateAsync()
+    protected override async Task DoWorkAsync(CancellationToken cancellationToken)
     {
         try
         {
-            using IServiceScope scope = _services.CreateScope();
+            using IServiceScope scope = serviceProvider.CreateScope();
             var usageService = scope.ServiceProvider.GetRequiredService<IUsageService>();
             await usageService.UpdateUsersCountAsync();
         }
         catch (Exception e)
         {
-            _logger.LogError("Failed to update usage statistics: {error}", e.Message);
+            logger.LogCritical(e, $"{nameof(UserCountUpdaterBackgroundService)} failed.");
         }
     }
 }
