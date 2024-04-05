cb8d695f0581af46ad27f1d9c0940fe370802ccf
jrmccannon <jared.r.mccannon@gmail.com>
2024-04-05 16:05:46 -0500

    Put it in both tenants

--- src/Service/Storage/Ef/DbGlobalMsSqlContext.cs
+++ src/Service/Storage/Ef/DbGlobalMsSqlContext.cs
@@ -9,7 +9,7 @@ public class DbGlobalMsSqlContext : DbGlobalContext
         : base(options)
     {
     }
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
--- src/Service/Storage/Ef/DbTenantMsSqlContext.cs
+++ src/Service/Storage/Ef/DbTenantMsSqlContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Passwordless.Service.Models;
 
 namespace Passwordless.Service.Storage.Ef;
 
@@ -7,5 +8,14 @@ public class DbTenantMsSqlContext : DbTenantContext
     public DbTenantMsSqlContext(DbContextOptions<DbTenantMsSqlContext> options, ITenantProvider tenantProvider)
         : base(options, tenantProvider)
     {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<AuthenticationConfiguration>()
+            .Property(c => c.TimeToLive)
+            .HasConversion(c => c.Ticks, c => TimeSpan.FromTicks(c));
     }
 }
