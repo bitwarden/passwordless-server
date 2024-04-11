e23dc0e265ac1d661fd3729f5946f9e4b1cce553
jrmccannon <jared.r.mccannon@gmail.com>
2024-04-11 10:38:38 -0500

    formatting.

--- src/Service/Models/SignInVerifyDTO.cs
+++ src/Service/Models/SignInVerifyDTO.cs
@@ -7,7 +7,7 @@ namespace Passwordless.Service.Models;
 public class SignInVerifyDTO : RequestBase
 {
     public string Token { get; set; }
-    
+
     [JsonConverter(typeof(SignInPurposeConverter))]
     public SignInPurpose Purpose { get; set; } = SignInPurposes.SignIn;
 }
