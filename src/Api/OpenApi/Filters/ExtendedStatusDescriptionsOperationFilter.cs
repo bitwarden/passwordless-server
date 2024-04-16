e5fcb255db5a34c4a73ffe305dc0e3e1fed6be73
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2024-04-16 22:22:32 +0300

    dotnet format sucks, can we just use csharpier please?

--- src/Api/OpenApi/Filters/ExtendedStatusDescriptionsOperationFilter.cs
+++ src/Api/OpenApi/Filters/ExtendedStatusDescriptionsOperationFilter.cs
@@ -17,50 +17,50 @@ public class ExtendedStatusDescriptionsOperationFilter : IOperationFilter
             switch (response.Key)
             {
                 case "200":
-                {
-                    response.Value.Description = "\u2705 Everything is OK.";
-                    break;
-                }
+                    {
+                        response.Value.Description = "\u2705 Everything is OK.";
+                        break;
+                    }
                 case "201":
-                {
-                    response.Value.Description = "\u2705 Everything is OK, resource created.";
-                    break;
-                }
+                    {
+                        response.Value.Description = "\u2705 Everything is OK, resource created.";
+                        break;
+                    }
                 case "204":
-                {
-                    response.Value.Description = "\u2705 Everything is OK, response is empty.";
-                    break;
-                }
+                    {
+                        response.Value.Description = "\u2705 Everything is OK, response is empty.";
+                        break;
+                    }
                 case "400":
-                {
-                    response.Value.Description = "\ud83d\udd34 Bad request, see problem details for more info.";
-                    break;
-                }
+                    {
+                        response.Value.Description = "\ud83d\udd34 Bad request, see problem details for more info.";
+                        break;
+                    }
                 case "401":
-                {
-                    response.Value.Description = "\ud83d\udd34 You did not identify yourself.";
-                    break;
-                }
+                    {
+                        response.Value.Description = "\ud83d\udd34 You did not identify yourself.";
+                        break;
+                    }
                 case "403":
-                {
-                    response.Value.Description = "\ud83d\udd34 You are not allowed to perform the action, see problem details for more info.";
-                    break;
-                }
+                    {
+                        response.Value.Description = "\ud83d\udd34 You are not allowed to perform the action, see problem details for more info.";
+                        break;
+                    }
                 case "409":
-                {
-                    response.Value.Description = "\ud83d\udd34 Conflict, see problem details for more info.";
-                    break;
-                }
+                    {
+                        response.Value.Description = "\ud83d\udd34 Conflict, see problem details for more info.";
+                        break;
+                    }
                 case "429":
-                {
-                    response.Value.Description = "\ud83d\udd34 Too many requests, see problem details for more info.";
-                    break;
-                }
+                    {
+                        response.Value.Description = "\ud83d\udd34 Too many requests, see problem details for more info.";
+                        break;
+                    }
                 case "500":
-                {
-                    response.Value.Description = "\ud83d\udd34 Something went wrong on our side.";
-                    break;
-                }
+                    {
+                        response.Value.Description = "\ud83d\udd34 Something went wrong on our side.";
+                        break;
+                    }
             }
         }
     }
