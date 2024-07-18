9a79e74bccfad00886ea195ed12af26ece1243c7
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-07-18 21:10:28 +0200

    Fix magic links lifecycle

---
+++ src/AdminConsole/Components/Pages/App/Settings/SettingsComponents/MagicLinksSection.razor.cs
@@ -0,0 +1,51 @@
+using System.Web;
+using Microsoft.AspNetCore.Components;
+using Passwordless.Common.Models.Apps;
+
+namespace Passwordless.AdminConsole.Components.Pages.App.Settings.SettingsComponents;
+
+public partial class MagicLinksSection : ComponentBase
+{
+    public const string FormName = "magic-links-form";
+
+    [SupplyParameterFromForm(FormName = FormName)]
+    public SaveFormModel? Form { get; set; }
+
+    public class SaveFormModel
+    {
+        public bool IsEnabled { get; set; }
+    }
+
+    protected override void OnInitialized()
+    {
+        Form ??= new SaveFormModel { IsEnabled = CurrentContext.Features.IsMagicLinksEnabled };
+    }
+
+    private async Task OnFormSubmittedAsync()
+    {
+        static bool? GetFinalValue(bool originalValue, bool postedValue) =>
+            originalValue == postedValue ? null : postedValue;
+
+        var name = HttpContextAccessor.HttpContext!.User.Identity!.Name;
+        if (string.IsNullOrWhiteSpace(CurrentContext.AppId) || string.IsNullOrWhiteSpace(name))
+        {
+            NavigationManager.NavigateTo($"/Error?Message={HttpUtility.UrlEncode("Something unexpected happened.")}");
+            return;
+        }
+
+        try
+        {
+            await ScopedPasswordlessClient.SetFeaturesAsync(new SetFeaturesRequest
+            {
+                PerformedBy = name,
+                EnableMagicLinks = GetFinalValue(CurrentContext.Features.IsMagicLinksEnabled, Form.IsEnabled)
+            });
+            NavigationManager.Refresh();
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, "Failed to save settings for {appId}", CurrentContext.AppId);
+            NavigationManager.NavigateTo($"/Error?Message={HttpUtility.UrlEncode(ex.Message)}");
+        }
+    }
+}
---
+++ src/AdminConsole/Components/Pages/App/Settings/SettingsComponents/ManuallyGeneratedAuthenticationTokensSection.razor.cs
@@ -0,0 +1,50 @@
+using System.Web;
+using Microsoft.AspNetCore.Components;
+using Passwordless.Common.Models.Apps;
+
+namespace Passwordless.AdminConsole.Components.Pages.App.Settings.SettingsComponents;
+
+public partial class ManuallyGeneratedAuthenticationTokensSection : ComponentBase
+{
+    public const string FormName = "manually-generated-authentication-tokens-form";
+
+    public SaveFormModel? Form { get; set; }
+
+    protected override void OnInitialized()
+    {
+        Form ??= new SaveFormModel { IsEnabled = CurrentContext.Features.IsGenerateSignInTokenEndpointEnabled };
+    }
+
+    private async Task OnFormSubmittedAsync()
+    {
+        static bool? GetFinalValue(bool originalValue, bool postedValue) =>
+            originalValue == postedValue ? null : postedValue;
+
+        var name = HttpContextAccessor.HttpContext!.User.Identity!.Name;
+        if (string.IsNullOrWhiteSpace(CurrentContext.AppId) || string.IsNullOrWhiteSpace(name))
+        {
+            NavigationManager.NavigateTo($"/Error?Message={HttpUtility.UrlEncode("Something unexpected happened.")}");
+            return;
+        }
+
+        try
+        {
+            await ScopedPasswordlessClient.SetFeaturesAsync(new SetFeaturesRequest
+            {
+                PerformedBy = name,
+                EnableManuallyGeneratedAuthenticationTokens = GetFinalValue(CurrentContext.Features.IsGenerateSignInTokenEndpointEnabled, Form!.IsEnabled)
+            });
+            NavigationManager.Refresh();
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, "Failed to save settings for {appId}", CurrentContext.AppId);
+            NavigationManager.NavigateTo($"/Error?Message={HttpUtility.UrlEncode(ex.Message)}");
+        }
+    }
+
+    public class SaveFormModel
+    {
+        public bool IsEnabled { get; set; }
+    }
+}
