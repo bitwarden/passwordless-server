3fbe3752cc09ba8437edb5c1599903ebfa51fc31
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-07-18 21:15:42 +0200

    fix for Razor Pages

--- src/AdminConsole/Components/Pages/App/Settings/SettingsComponents/MagicLinksSection.razor.cs
+++ src/AdminConsole/Components/Pages/App/Settings/SettingsComponents/MagicLinksSection.razor.cs
@@ -1,5 +1,6 @@
 using System.Web;
 using Microsoft.AspNetCore.Components;
+using Passwordless.AdminConsole.Helpers;
 using Passwordless.Common.Models.Apps;
 
 namespace Passwordless.AdminConsole.Components.Pages.App.Settings.SettingsComponents;
@@ -16,9 +17,22 @@ public partial class MagicLinksSection : ComponentBase
         public bool IsEnabled { get; set; }
     }
 
-    protected override void OnInitialized()
+    protected override async Task OnInitializedAsync()
     {
         Form ??= new SaveFormModel { IsEnabled = CurrentContext.Features.IsMagicLinksEnabled };
+
+        // If we've posted a form, we need to add backwards compatibility for Razor Pages. Bind it to the model, and trigger the form submission handler.
+        if (HttpContextAccessor.IsRazorPages() && HttpContextAccessor.HttpContext!.Request.HasFormContentType)
+        {
+            var request = HttpContextAccessor.HttpContext!.Request;
+            switch (request.Form["_handler"])
+            {
+                case FormName:
+                    Form.IsEnabled = bool.Parse(request.Form["Form.IsEnabled"]!);
+                    await OnFormSubmittedAsync();
+                    break;
+            }
+        }
     }
 
     private async Task OnFormSubmittedAsync()
