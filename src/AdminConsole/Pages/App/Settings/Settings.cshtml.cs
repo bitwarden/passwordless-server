89d3ca5a2fad5ba13dd7cdf8212643fe7fb16862
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-07-19 15:06:49 +0200

    PAS-479 | Fix magic links section lifecycle when locking/unlocking API keys (#641)

--- src/AdminConsole/Pages/App/Settings/Settings.cshtml.cs
+++ src/AdminConsole/Pages/App/Settings/Settings.cshtml.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Passwordless.AdminConsole.Middleware;
 using Passwordless.AdminConsole.Services;
-using Passwordless.Common.Models.Apps;
 using Application = Passwordless.AdminConsole.Models.Application;
 
 namespace Passwordless.AdminConsole.Pages.App.Settings;
@@ -13,20 +12,17 @@ public class SettingsModel : BaseExtendedPageModel
     private readonly IDataService _dataService;
     private readonly ICurrentContext _currentContext;
     private readonly IApplicationService _appService;
-    private readonly IScopedPasswordlessClient _scopedPasswordlessClient;
 
     public SettingsModel(
         ILogger<SettingsModel> logger,
         IDataService dataService,
         ICurrentContext currentContext,
-        IApplicationService appService,
-        IScopedPasswordlessClient scopedPasswordlessClient)
+        IApplicationService appService)
     {
         _logger = logger;
         _dataService = dataService;
         _currentContext = currentContext;
         _appService = appService;
-        _scopedPasswordlessClient = scopedPasswordlessClient;
     }
 
     public Models.Organization Organization { get; set; }
@@ -41,12 +37,6 @@ public class SettingsModel : BaseExtendedPageModel
 
     public bool CanDeleteImmediately { get; private set; }
 
-    [BindProperty]
-    public bool IsManualTokenGenerationEnabled { get; set; }
-
-    [BindProperty]
-    public bool IsMagicLinksEnabled { get; set; }
-
     private async Task InitializeAsync()
     {
         Organization = await _dataService.GetOrganizationWithDataAsync();
@@ -56,9 +46,6 @@ public class SettingsModel : BaseExtendedPageModel
 
         Application = application ?? throw new InvalidOperationException("Application not found.");
         CanDeleteImmediately = await _appService.CanDeleteApplicationImmediatelyAsync(ApplicationId);
-
-        IsManualTokenGenerationEnabled = _currentContext.Features.IsGenerateSignInTokenEndpointEnabled;
-        IsMagicLinksEnabled = _currentContext.Features.IsMagicLinksEnabled;
     }
 
     public async Task OnGet()
@@ -119,35 +106,6 @@ public class SettingsModel : BaseExtendedPageModel
         catch (Exception ex)
         {
             _logger.LogError("Failed to cancel application deletion for application: {appId}", applicationId);
-            return RedirectToPage("/Error", new { ex.Message });
-        }
-    }
-
-    public async Task<IActionResult> OnPostSettingsAsync()
-    {
-        static bool? GetFinalValue(bool originalValue, bool postedValue) =>
-            originalValue == postedValue ? null : postedValue;
-
-        if (string.IsNullOrWhiteSpace(_currentContext.AppId) || string.IsNullOrWhiteSpace(User.Identity?.Name))
-        {
-            return RedirectToPage("/Error", new { Message = "Something unexpected happened." });
-        }
-
-        try
-        {
-            await _scopedPasswordlessClient.SetFeaturesAsync(new SetFeaturesRequest
-            {
-                PerformedBy = User.Identity!.Name,
-                EnableManuallyGeneratedAuthenticationTokens =
-                    GetFinalValue(_currentContext.Features.IsGenerateSignInTokenEndpointEnabled, IsManualTokenGenerationEnabled),
-                EnableMagicLinks = GetFinalValue(_currentContext.Features.IsMagicLinksEnabled, IsMagicLinksEnabled)
-            });
-
-            return RedirectToPage();
-        }
-        catch (Exception ex)
-        {
-            _logger.LogError(ex, "Failed to save settings for {appId}", _currentContext.AppId);
             return RedirectToPage("/Error", new { ex.Message });
         }
     }
