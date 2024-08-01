c2d02cf4411762f146808346220b48fe233a2980
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-08-01 15:47:18 +0200

    PAS-380 | Focus when the dialogs/modals open on the form/input elements (#655)

---
+++ src/AdminConsole/wwwroot/js/composables/useFocusTrap.mjs
@@ -0,0 +1,65 @@
+import { customRef } from "vue";
+
+const focusableElementsSelector =
+    'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+
+const useFocusTrap = () => {
+    let focusableElements = [];
+    let $firstFocusable;
+    let $lastFocusable;
+    const trapRef = customRef((track, trigger) => {
+        let $trapEl = null;
+        return {
+            get() {
+                track();
+                return $trapEl;
+            },
+            set(value) {
+                $trapEl = value;
+                value ? initFocusTrap() : clearFocusTrap();
+                trigger();
+            },
+        };
+    });
+
+    function keyHandler(e) {
+        const isTabPressed = e.key === "Tab";
+
+        if (!isTabPressed) return;
+
+        if (e.shiftKey) {
+            if (document.activeElement === $firstFocusable) {
+                $lastFocusable.focus();
+                e.preventDefault();
+            }
+        } else {
+            if (document.activeElement === $lastFocusable) {
+                $firstFocusable.focus();
+                e.preventDefault();
+            }
+        }
+    }
+
+    function initFocusTrap() {
+        if (!trapRef.value) return;
+        focusableElements = trapRef.value.querySelectorAll(
+            focusableElementsSelector
+        );
+        $firstFocusable = focusableElements[0];
+        $lastFocusable = focusableElements[focusableElements.length - 1];
+        document.addEventListener("keydown", keyHandler);
+        $firstFocusable.focus();
+    }
+
+    function clearFocusTrap() {
+        document.removeEventListener("keydown", keyHandler);
+    }
+
+    return {
+        trapRef,
+        initFocusTrap,
+        clearFocusTrap,
+    };
+};
+
+export default useFocusTrap;
