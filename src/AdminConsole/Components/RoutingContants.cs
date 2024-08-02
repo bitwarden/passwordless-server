a93c452cf604f9eb09c43b3dcc063b3716f34fc9
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-08-02 13:37:02 +0200

    wip

--- src/AdminConsole/Components/RoutingContants.cs
+++ src/AdminConsole/Components/RoutingContants.cs
@@ -2,6 +2,17 @@ namespace Passwordless.AdminConsole.Components;
 
 public static class RoutingContants
 {
+    public const string Initialize = "/initialize";
+    public const string SignUp = "/signup";
+
+    public static class Account
+    {
+        public const string Root = "/account";
+
+        public const string AccessDenied = $"{Root}/accessdenied";
+        public const string Logout = $"{Root}/logout";
+    }
+
     public static class Billing
     {
         public const string Root = "/billing";
@@ -11,5 +22,15 @@ public static class RoutingContants
         public const string Invoices = $"{Root}/invoices";
         public const string Manage = $"{Root}/manage";
         public const string Success = $"{Root}/success";
+    }
+
+    public static class Organization
+    {
+        public const string Root = "/organization";
+
+        public const string Create = $"{Root}/create";
+        public const string Admins = $"{Root}/admins";
+        public const string Log = $"{Root}/log";
+        public const string Settings = $"{Root}/settings";
     }
 }
