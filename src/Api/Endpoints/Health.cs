d071c573f0d6bff4ed4667f480250e6081bbed26
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-08-12 10:10:05 +0200

    WIP

--- src/Api/Endpoints/Health.cs
+++ src/Api/Endpoints/Health.cs
@@ -1,7 +1,3 @@
-using MailKit;
-using Microsoft.Extensions.Options;
-using Passwordless.Common.Services.Mail;
-using Passwordless.Common.Services.Mail.SendGrid;
 using Passwordless.Service.Helpers;
 
 namespace Passwordless.Api.Endpoints;
@@ -12,8 +8,5 @@ public static class HealthEndpoints
     {
         app.MapGet("health/throw/api", (ctx) => throw new ApiException("test_error", "Testing error response", 400));
         app.MapGet("health/throw/exception", (ctx) => throw new Exception("Testing error response", new Exception("Inner exception")));
-        app.MapGet("health/mail-test", async (IOptionsMonitor<MailConfiguration> config) =>
-        {
-        });
     }
 }
