36fe36801192191e2efa4d9c0bb80a7a7f7448bf
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-08-17 19:19:22 +0200

    PAS-510 | Hide application settings sections when pending deletion

--- src/AdminConsole/Components/Pages/App/Settings/Settings.razor.cs
+++ src/AdminConsole/Components/Pages/App/Settings/Settings.razor.cs
@@ -4,9 +4,19 @@ namespace Passwordless.AdminConsole.Components.Pages.App.Settings;
 
 public partial class Settings : BaseApplicationPage
 {
-    public bool IsAttestationAllowed => CurrentContext.Features.AllowAttestation;
-
     public Application? Application { get; private set; }
+
+    public bool ShowPlanSection => !CurrentContext.IsPendingDelete;
+
+    public bool ShowApiKeysSection => !CurrentContext.IsPendingDelete;
+
+    public bool ShowMagicLinksSection => !CurrentContext.IsPendingDelete;
+
+    public bool ShowAttestationSection => !CurrentContext.IsPendingDelete && CurrentContext.Features.AllowAttestation;
+
+    public bool ShowManuallyGeneratedAuthenticationTokensSection => !CurrentContext.IsPendingDelete;
+
+    public bool ShowAuthenticationConfigurationSection => !CurrentContext.IsPendingDelete;
 
     protected override async Task OnInitializedAsync()
     {
