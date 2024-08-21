b4ff02bf8c008704bce9feee1612e8d01a2d6ce8
Jonas Hendrickx <jhendrickx@bitwarden.com>
2024-08-21 16:03:34 +0200

    Wait Strategy needs to be changed for TestContainers

--- tests/Api.IntegrationTests/PasswordlessApiFixture.cs
+++ tests/Api.IntegrationTests/PasswordlessApiFixture.cs
@@ -1,3 +1,4 @@
+using DotNet.Testcontainers.Builders;
 using Testcontainers.MsSql;
 using Xunit;
 
@@ -5,7 +6,10 @@ namespace Passwordless.Api.IntegrationTests;
 
 public class PasswordlessApiFixture : IAsyncDisposable, IAsyncLifetime
 {
-    private readonly MsSqlContainer _dbContainer = new MsSqlBuilder().Build();
+    private readonly MsSqlContainer _dbContainer = new MsSqlBuilder()
+        .WithImage("mcr.microsoft.com/mssql/server:2022-CU14-ubuntu-22.04")
+        .WithWaitStrategy(Wait.ForUnixContainer().UntilCommandIsCompleted("/opt/mssql-tools18/bin/sqlcmd", "-C", "-Q", "SELECT 1;"))
+        .Build();
 
     public async Task InitializeAsync() => await _dbContainer.StartAsync();
 
