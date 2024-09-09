938530d7e49b302bffcc02cd77763a467e62e460
Oleksii Holub <1935960+Tyrrrz@users.noreply.github.com>
2024-09-10 00:27:33 +0300

    format schformat

--- src/Service/Migrations/Mssql/20240909154123_AuthenticatorDisplayName.cs
+++ src/Service/Migrations/Mssql/20240909154123_AuthenticatorDisplayName.cs
@@ -2,27 +2,26 @@
 
 #nullable disable
 
-namespace Passwordless.Service.Migrations.Mssql
+namespace Passwordless.Service.Migrations.Mssql;
+
+/// <inheritdoc />
+public partial class AuthenticatorDisplayName : Migration
 {
     /// <inheritdoc />
-    public partial class AuthenticatorDisplayName : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<string>(
-                name: "AuthenticatorDisplayName",
-                table: "Credentials",
-                type: "nvarchar(max)",
-                nullable: true);
-        }
+        migrationBuilder.AddColumn<string>(
+            name: "AuthenticatorDisplayName",
+            table: "Credentials",
+            type: "nvarchar(max)",
+            nullable: true);
+    }
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "AuthenticatorDisplayName",
-                table: "Credentials");
-        }
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropColumn(
+            name: "AuthenticatorDisplayName",
+            table: "Credentials");
     }
 }
