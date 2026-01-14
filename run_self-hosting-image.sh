2cb66e626da01bd996aec4dc97e07d87260bec91
Vince Grassia <593223+vgrassia@users.noreply.github.com>
2026-01-14 12:23:27 -0500

    Update example domain to one specified by the IANA for testing.

--- run_self-hosting-image.sh
+++ run_self-hosting-image.sh
@@ -5,7 +5,7 @@ docker build . -f self-host/Dockerfile -t bitwarden/passwordless
 docker run \
   --name passwordless \
   -p 5701:5701 \
-  -e BWP_DOMAIN=yourdomain.com \
+  -e BWP_DOMAIN=example.com \
   -e BWP_ENABLE_SSL=false \
   -v /your/path/passwordless-self-hosting:/etc/bitwarden_passwordless \
   bitwarden/passwordless
